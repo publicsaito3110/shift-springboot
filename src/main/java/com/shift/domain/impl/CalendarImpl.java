5b0108a03762769aeffe941c4dc96b4a353746a5
publicsaito3110 <saito.ad.ac3110@gmail.com>
2022-06-22 10:08:23 +0900

    fix: 年月取得処理を追加

--- src/main/java/com/shift/domain/impl/CalendarImpl.java
+++ src/main/java/com/shift/domain/impl/CalendarImpl.java
@@ -12,30 +12,48 @@ import com.shift.domain.Interface.CalendarIntarface;
 @Component
 public class CalendarImpl implements CalendarIntarface {
 
-	//現在の日付を取得
+	//フィールド
 	private int year;
 	private int month;
 
 
 	@Override
-	public void getDay(HttpServletRequest request, HttpServletResponse response) {
+	public void getMonth(HttpServletRequest request, HttpServletResponse response) {
+
+		//パラメーターを受け取る
+		String ym = request.getParameter("ym");
+
+		//ym(年月)が指定されていたとき
+		if (ym != null) {
+
+			//ymからyear(4文字)month(2文字)を取得
+			this.year  = Integer.parseInt(ym.substring(0, 4));
+			this.month = Integer.parseInt(ym.substring(4, 6));
+
+			// 引き渡す値を設定
+			request.setAttribute("year", this.year);
+			request.setAttribute("month", this.month);
+			return;
+		}
 
 		//現在の日付を取得
 		LocalDate now = LocalDate.now();
-		int year = now.getYear();
-		int month = now.getMonthValue();
+		this.year = now.getYear();
+		this.month = now.getMonthValue();
 
-		this.year = 20220601;
-
+		// 引き渡す値を設定
+		request.setAttribute("year", this.year);
+		request.setAttribute("month", this.month);
 	}
+
+
 	@Override
 	public void getSchedule(HttpServletRequest request, HttpServletResponse response) {
 
-		request.setAttribute("schedule", "OK");
 	}
 	@Override
 	public void generateCalendar(HttpServletRequest request, HttpServletResponse response) {
 
-		request.setAttribute("carlendar", this.year);
+		request.setAttribute("carlendar", this.year + "年" + this.month + "月");
 	}
 }
