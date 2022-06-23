938d88aaba68d0ddd639e7fa4cae16e939feddf0
publicsaito3110 <saito.ad.ac3110@gmail.com>
2022-06-23 14:48:17 +0900

    add; Calendar Beanの追加

---
+++ src/main/java/com/shift/bean/ScheduleBean.java
@@ -0,0 +1,75 @@
+package com.shift.bean;
+
+public class ScheduleBean {
+
+
+	//フィールド
+	private String day;
+	private String ymd;
+
+	private String user1;
+	private String user2;
+	private String user3;
+	private String memo1;
+	private String memo2;
+	private String memo3;
+
+	private String typeIUD;
+
+
+	// getter, setter
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getYmd() {
+		return ymd;
+	}
+	public void setYmd(String ymd) {
+		this.ymd = ymd;
+	}
+	public String getUser1() {
+		return user1;
+	}
+	public void setUser1(String user1) {
+		this.user1 = user1;
+	}
+	public String getUser2() {
+		return user2;
+	}
+	public void setUser2(String user2) {
+		this.user2 = user2;
+	}
+	public String getUser3() {
+		return user3;
+	}
+	public void setUser3(String user3) {
+		this.user3 = user3;
+	}
+	public String getMemo1() {
+		return memo1;
+	}
+	public void setMemo1(String memo1) {
+		this.memo1 = memo1;
+	}
+	public String getMemo2() {
+		return memo2;
+	}
+	public void setMemo2(String memo2) {
+		this.memo2 = memo2;
+	}
+	public String getMemo3() {
+		return memo3;
+	}
+	public void setMemo3(String memo3) {
+		this.memo3 = memo3;
+	}
+	public String getTypeIUD() {
+		return typeIUD;
+	}
+	public void setTypeIUD(String typeIUD) {
+		this.typeIUD = typeIUD;
+	}
+}
