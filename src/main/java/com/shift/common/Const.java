5f94c4e24a44564e563a01232c93df60f5b2000c
publicsaito3110 <saito.ad.ac3110@gmail.com>
2022-06-27 10:58:45 +0900

    fix: 定数の追加

--- src/main/java/com/shift/common/Const.java
+++ src/main/java/com/shift/common/Const.java
@@ -4,4 +4,10 @@ public class Const {
 	private Const() {
 		//インスタンス化の禁止
 	}
+
+	//-----------
+	//Calendar
+	//-----------
+	public static final String HTML_CLASS_CALENDAR_SAT = "calendar-saturday";
+	public static final String HTML_CLASS_CALENDAR_SUN = "calendar-sunday";
 }
