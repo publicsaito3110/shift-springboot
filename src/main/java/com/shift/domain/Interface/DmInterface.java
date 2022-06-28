7b252c57493b85faab55fae1a3a0cea1c32718a5
publicsaito3110 <public.business.saito3110@gmail.com>
2022-06-28 16:40:35 +0900

    add: DM追加に伴うinterfaceの追加

---
+++ src/main/java/com/shift/domain/Interface/DmInterface.java
@@ -0,0 +1,10 @@
+package com.shift.domain.Interface;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public interface DmInterface extends BaseInterface{
+
+	void getUserIdBySession(ModelAndView modelAndView);
+
+	void getDmHistory(ModelAndView modelAndView);
+}
