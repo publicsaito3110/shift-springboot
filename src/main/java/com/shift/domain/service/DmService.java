84397577ac50c40bda50080fe12e0f7fb3246b7e
publicsaito3110 <public.business.saito3110@gmail.com>
2022-06-28 16:43:53 +0900

    add: DM追加に伴うServiceの追加

---
+++ src/main/java/com/shift/domain/service/DmService.java
@@ -0,0 +1,27 @@
+package com.shift.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shift.domain.Interface.DmInterface;
+
+@Component
+public class DmService{
+
+	@Autowired
+	DmInterface dmIntarface;
+
+
+	public DmService(DmInterface dmIntarface) {
+		super();
+		this.dmIntarface = dmIntarface;
+	}
+
+
+	public void dm(ModelAndView modelAndView) {
+
+		dmIntarface.getUserIdBySession(modelAndView);
+		dmIntarface.getDmHistory(modelAndView);
+	}
+}
