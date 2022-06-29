059b47e042899e802815ed2355d23d0be475910f
publicsaito3110 <public.business.saito3110@gmail.com>
2022-06-29 10:12:57 +0900

    add: トーク機能追加に伴うImpleの追加

---
+++ src/main/java/com/shift/domain/impl/DmTalkImpl.java
@@ -0,0 +1,40 @@
+package com.shift.domain.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shift.domain.Interface.DmTalkInterface;
+import com.shift.domain.repositry.DmRepositry;
+import com.shift.entity.DmEntity;
+
+@Component
+public class DmTalkImpl implements DmTalkInterface {
+
+
+	//フィールド
+	private String id;
+
+	@Autowired
+	DmRepositry dmRepositry;
+
+
+	@Override
+	public void getgetSessionByLoginUser(ModelAndView modelAndView) {
+
+		String id = "A001";
+		this.id = id;
+
+	}
+	@Override
+	public void getTalkHistoryByReceiveUser(ModelAndView modelAndView, String receiveUser) {
+
+		List<DmEntity> talkHistoryList = new ArrayList<>();
+		talkHistoryList = dmRepositry.selectTalkHistoryByLoginUserReceiveUser(this.id, receiveUser);
+
+		modelAndView.addObject("talkHistoryList", talkHistoryList);
+	}
+}
