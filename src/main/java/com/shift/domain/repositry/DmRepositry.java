99b76740e17b09754e04506193366f02d45c1035
publicsaito3110 <public.business.saito3110@gmail.com>
2022-06-29 10:11:07 +0900

    fix: トーク履歴取得によるRowMapperの追加

--- src/main/java/com/shift/domain/repositry/DmRepositry.java
+++ src/main/java/com/shift/domain/repositry/DmRepositry.java
@@ -19,7 +19,7 @@ public class DmRepositry {
 	JdbcTemplate jdbcTemplate;
 
 
-	class ScheduleRowMapper implements RowMapper<DmEntity> {
+	class DmMenueRowMapper implements RowMapper<DmEntity> {
 
 		@Override
 		public DmEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -36,8 +36,22 @@ public class DmRepositry {
 		}
 	}
 
+	class DmTalkRowMapper implements RowMapper<DmEntity> {
 
-	public List<DmEntity> selectDmTalkHistoryByYm(String id) {
+		@Override
+		public DmEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+			DmEntity dmEntity = new DmEntity();
+			dmEntity.setMsg(rs.getString("msg"));
+			dmEntity.setSendUser(rs.getString("send_user"));
+			dmEntity.setMsgDate(rs.getString("msg_date"));
+
+			return dmEntity;
+		}
+	}
+
+
+	public List<DmEntity> selectDmTalkHistoryByLoginUser(String loginUser) {
 
 		//SQL
 		StringBuffer sqlSb = new StringBuffer();
@@ -46,13 +60,27 @@ public class DmRepositry {
 		sqlSb.append(" SELECT DISTINCT d.msg_date, d.msg, d.send_user, d.receive_user ");
 		sqlSb.append(" , CASE WHEN d.send_user = ? THEN s.name WHEN d.receive_user = ? THEN u.name END AS msg_to_name ");
 		sqlSb.append(" , CASE WHEN d.send_user = ? THEN s.id WHEN d.receive_user = ? THEN u.id END AS msg_to_id ");
-		sqlSb.append(" FROM (DM d INNER JOIN USER u ON u.id = d.send_user) INNER JOIN USER s ON s.id = d.receive_user  ");
+		sqlSb.append(" FROM (dm d INNER JOIN USER u ON u.id = d.send_user) INNER JOIN USER s ON s.id = d.receive_user  ");
 		sqlSb.append(" WHERE d.send_user = ? OR d.receive_user = ? ");
 		sqlSb.append(" ORDER BY d.id DESC ");
 		sqlSb.append(" ) a ");
-		sqlSb.append(" GROUP BY a.MSG_TO_ID ");
+		sqlSb.append(" GROUP BY a.msg_to_id ");
 		sqlSb.append(" ; ");
 
-		return jdbcTemplate.query(sqlSb.toString(), new Object[] {id, id, id, id, id, id}, new ScheduleRowMapper());
+		return jdbcTemplate.query(sqlSb.toString(), new Object[] {loginUser, loginUser, loginUser, loginUser, loginUser, loginUser}, new DmMenueRowMapper());
+	}
+
+
+	public List<DmEntity> selectTalkHistoryByLoginUserReceiveUser(String loginUser, String receiveUser) {
+
+		//SQL
+		StringBuffer sqlSb = new StringBuffer();
+		sqlSb.append("SELECT d.msg_date, d.msg, CASE WHEN d.send_user = ? THEN 'login-user' WHEN d.send_user != ? THEN 'not-login-user' END AS send_user ");
+		sqlSb.append(" FROM dm d ");
+		sqlSb.append(" WHERE d.send_user = ? AND d.receive_user = ? OR d.send_user = ? AND d.receive_user = ? ");
+		sqlSb.append(" ORDER BY d.id ");
+		sqlSb.append(" ; ");
+
+		return jdbcTemplate.query(sqlSb.toString(), new Object[] {loginUser, loginUser, loginUser, receiveUser, receiveUser, loginUser}, new DmTalkRowMapper());
 	}
 }
