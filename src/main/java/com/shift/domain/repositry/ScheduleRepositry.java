b5e767c88866cd07706c59058d9fd4f76fd1d52b
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-02 14:56:35 +0900

    fix: Spring JPA及びLombok追加に伴う修正

--- src/main/java/com/shift/domain/repositry/ScheduleRepositry.java
+++ src/main/java/com/shift/domain/repositry/ScheduleRepositry.java
@@ -1,46 +1,15 @@
 package com.shift.domain.repositry;
 
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shift.entity.ScheduleEntity;
 
 @Repository
-public class ScheduleRepositry {
+public interface ScheduleRepositry extends JpaRepository<ScheduleEntity, String> {
 
-	@Autowired
-	JdbcTemplate jdbcTemplate;
-
-
-	class ScheduleRowMapper implements RowMapper<ScheduleEntity> {
-
-		@Override
-		public ScheduleEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
-
-			ScheduleEntity scheduleEntity = new ScheduleEntity();
-			scheduleEntity.setYmd(rs.getString("ymd"));
-			scheduleEntity.setUser1(rs.getString("user1"));
-			scheduleEntity.setMemo1(rs.getString("memo1"));
-			scheduleEntity.setUser2(rs.getString("user2"));
-			scheduleEntity.setMemo2(rs.getString("memo2"));
-			scheduleEntity.setUser3(rs.getString("user3"));
-			scheduleEntity.setMemo3(rs.getString("memo3"));
-
-			return scheduleEntity;
-		}
-	}
-
-
-	public List<ScheduleEntity> selectScheduleMonthByYm(String ym) {
-
-		String paramYm = ym + "%";
-		return jdbcTemplate.query("SELECT * FROM schedule WHERE ymd LIKE ? ORDER BY ymd", new Object[] {paramYm}, new ScheduleRowMapper());
-	}
+	List<ScheduleEntity> findByYmdLike(String ymd);
 }
