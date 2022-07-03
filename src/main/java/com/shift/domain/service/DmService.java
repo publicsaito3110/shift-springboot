6a3a97f1bad60f4d8ef27e430d1cc0a051b5d527
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-03 16:02:18 +0900

    fix: DM送信機能の追加に伴うServiceの修正

--- src/main/java/com/shift/domain/service/DmService.java
+++ src/main/java/com/shift/domain/service/DmService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.shift.domain.Interface.DmInterface;
+import com.shift.domain.Interface.DmSendInterface;
 import com.shift.domain.Interface.DmTalkInterface;
 
 @Component
@@ -16,11 +17,15 @@ public class DmService{
 	@Autowired
 	DmTalkInterface dmTalkInterface;
 
+	@Autowired
+	DmSendInterface dmSendInterface;
 
-	public DmService(DmInterface dmIntarface, DmTalkInterface dmTalkInterface) {
+
+	public DmService(DmInterface dmIntarface, DmTalkInterface dmTalkInterface, DmSendInterface dmSendInterface) {
 		super();
 		this.dmIntarface = dmIntarface;
 		this.dmTalkInterface = dmTalkInterface;
+		this.dmSendInterface = dmSendInterface;
 	}
 
 
@@ -30,9 +35,18 @@ public class DmService{
 		dmIntarface.getDmHistory(modelAndView);
 	}
 
+
 	public void talkHistory(ModelAndView modelAndView, String receiveUser) {
 
-		dmTalkInterface.getgetSessionByLoginUser(modelAndView);
+		dmTalkInterface.getSessionByLoginUser(modelAndView);
+		dmTalkInterface.getTalkHistoryByReceiveUser(modelAndView, receiveUser);
+	}
+
+
+	public void recordChatTalkHistory(ModelAndView modelAndView, String receiveUser, String msg) {
+
+		dmSendInterface.chatRecord(receiveUser, msg);
+		dmTalkInterface.getSessionByLoginUser(modelAndView);
 		dmTalkInterface.getTalkHistoryByReceiveUser(modelAndView, receiveUser);
 	}
 }
