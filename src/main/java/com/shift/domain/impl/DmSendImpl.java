e326c77a049aafeb5f22a91deb3a53c5d50808ac
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-03 16:06:29 +0900

    add: DM送信機能の処理を追加

---
+++ src/main/java/com/shift/domain/impl/DmSendImpl.java
@@ -0,0 +1,33 @@
+package com.shift.domain.impl;
+
+import java.sql.Timestamp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.shift.domain.Interface.DmSendInterface;
+import com.shift.domain.repositry.DmRepositry;
+import com.shift.entity.DmEntity;
+
+@Component
+public class DmSendImpl implements DmSendInterface {
+
+	@Autowired
+	DmRepositry dmRepositry;
+
+
+	@Override
+	public void chatRecord(String receiveUser, String msg) {
+
+		String loginUser = "A001";
+
+		DmEntity dmEntity = new DmEntity();
+		dmEntity.setSendUser(loginUser);
+		dmEntity.setReceiveUser(receiveUser);
+		dmEntity.setMsg(msg);
+		dmEntity.setMsgDate(new Timestamp(System.currentTimeMillis()).toString());
+		dmRepositry.save(dmEntity);
+	}
+
+
+}
