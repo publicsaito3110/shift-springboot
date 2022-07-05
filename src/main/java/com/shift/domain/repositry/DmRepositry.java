698b3dd154d20d53d4719db8fec1183e58b9a6b0
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-05 18:28:19 +0900

    fix: BaseRepositryを継承

--- src/main/java/com/shift/domain/repositry/DmRepositry.java
+++ src/main/java/com/shift/domain/repositry/DmRepositry.java
@@ -3,14 +3,13 @@ package com.shift.domain.repositry;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.shift.entity.DmEntity;
 
 @Repository
-public interface DmRepositry extends JpaRepository<DmEntity, Integer>{
+public interface DmRepositry extends BaseRepositry<DmEntity, Integer>{
 
 @Query(value = "SELECT d.id, d.msg_date, d.msg, CASE WHEN d.send_user = :loginUser THEN 'login-user' WHEN d.send_user != :loginUser THEN 'not-login-user' END AS send_user, d.receive_user FROM dm d WHERE d.send_user = :loginUser AND d.receive_user = :receiveUser OR d.send_user = :receiveUser AND d.receive_user = :loginUser ORDER BY d.id", nativeQuery = true)
 	public List<DmEntity> selectTalkHistoryByLoginUserReceiveUser(String loginUser, String receiveUser);
--- src/main/java/com/shift/domain/repositry/ScheduleRepositry.java
+++ src/main/java/com/shift/domain/repositry/ScheduleRepositry.java
@@ -3,13 +3,12 @@ package com.shift.domain.repositry;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shift.entity.ScheduleEntity;
 
 @Repository
-public interface ScheduleRepositry extends JpaRepository<ScheduleEntity, String> {
+public interface ScheduleRepositry extends BaseRepositry<ScheduleEntity, String> {
 
 	List<ScheduleEntity> findByYmdLike(String ymd);
 }
