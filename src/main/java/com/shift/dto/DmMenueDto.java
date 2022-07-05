284ed9b8afc2b7dcc16787ff6b3f917ff41aed99
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-05 18:15:19 +0900

    fix: DmMenueDtoのSQL修正に伴うfieldの修正

--- src/main/java/com/shift/dto/DmMenueDto.java
+++ src/main/java/com/shift/dto/DmMenueDto.java
@@ -18,17 +18,8 @@ public class DmMenueDto {
 	@Column(name = "id")
 	private Integer id;
 
-	@Column(name = "send_user")
-	private String sendUser;
-
-	@Column(name = "receive_user")
-	private String receiveUser;
-
 	@Column(name = "msg")
 	private String msg;
-
-	@Column(name = "msg_date")
-	private String msgDate;
 
 	@Column(name = "msg_to_name", unique = true)
 	private String msgToName;
