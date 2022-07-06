fba84fead4b727ebb75dd5756a613e18d2895552
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-06 19:48:44 +0900

    add: HomeInterfaceの追加

---
+++ src/main/java/com/shift/domain/Interface/HomeInterface.java
@@ -0,0 +1,8 @@
+package com.shift.domain.Interface;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public interface HomeInterface extends BaseInterface {
+
+	void getDate(ModelAndView modelAndView);
+}
