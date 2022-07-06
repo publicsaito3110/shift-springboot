576b87d3bf32910499442b43dcb1c74e4a1f7e29
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-06 19:47:58 +0900

    add: HomeImplの追加

---
+++ src/main/java/com/shift/domain/impl/HomeImpl.java
@@ -0,0 +1,17 @@
+package com.shift.domain.impl;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shift.domain.Interface.HomeInterface;
+
+@Component
+public class HomeImpl  extends BaseImple implements HomeInterface {
+
+	@Override
+	public void getDate(ModelAndView modelAndView) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
