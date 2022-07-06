ebbacd3435f45c16dafb91a04f5737a7a0db3c64
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-06 19:51:07 +0900

    add: NewsEntityの追加

---
+++ src/main/java/com/shift/entity/NewsEntity.java
@@ -0,0 +1,34 @@
+package com.shift.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="news")
+@Data
+public class NewsEntity {
+
+	//フィールド
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private String id;
+
+	@Column(name = "ymd")
+	private String ymd;
+
+	@Column(name = "category")
+	private String category;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "content")
+	private String content;
+}
