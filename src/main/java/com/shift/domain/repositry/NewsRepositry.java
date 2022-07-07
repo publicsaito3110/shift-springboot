b19d936700c5e1a460c26d4faf7d92b8aea1ea1c
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-07 19:47:54 +0900

    fix: RepositryにAnotation Queryを追加

--- src/main/java/com/shift/domain/repositry/NewsRepositry.java
+++ src/main/java/com/shift/domain/repositry/NewsRepositry.java
@@ -1,5 +1,8 @@
 package com.shift.domain.repositry;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.shift.entity.NewsEntity;
@@ -7,4 +10,6 @@ import com.shift.entity.NewsEntity;
 @Repository
 public interface NewsRepositry extends BaseRepositry<NewsEntity, Integer> {
 
+	@Query(value = "SELECT * FROM news WHERE ymd <= :nowYmd ORDER BY ymd DESC LIMIT :newsLimit",  nativeQuery = true)
+	List<NewsEntity> selectNewsBeforeNowByNowYmdNewsLimit(String nowYmd, int newsLimit);
 }
