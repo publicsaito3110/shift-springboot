292a04da01a03db3c7fe68233c74519e860b76bc
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-08 05:54:40 +0900

    fix: Lombokの追加

--- src/main/java/com/shift/bean/ScheduleBean.java
+++ src/main/java/com/shift/bean/ScheduleBean.java
@@ -1,5 +1,8 @@
 package com.shift.bean;
 
+import lombok.Data;
+
+@Data
 public class ScheduleBean {
 
 
@@ -14,61 +17,4 @@ public class ScheduleBean {
 
 	private String day;
 	private String htmlClass;
-
-
-	// getter, setter
-	public String getYmd() {
-		return ymd;
-	}
-	public void setYmd(String ymd) {
-		this.ymd = ymd;
-	}
-	public String getUser1() {
-		return user1;
-	}
-	public void setUser1(String user1) {
-		this.user1 = user1;
-	}
-	public String getUser2() {
-		return user2;
-	}
-	public void setUser2(String user2) {
-		this.user2 = user2;
-	}
-	public String getUser3() {
-		return user3;
-	}
-	public void setUser3(String user3) {
-		this.user3 = user3;
-	}
-	public String getMemo1() {
-		return memo1;
-	}
-	public void setMemo1(String memo1) {
-		this.memo1 = memo1;
-	}
-	public String getMemo2() {
-		return memo2;
-	}
-	public void setMemo2(String memo2) {
-		this.memo2 = memo2;
-	}
-	public String getMemo3() {
-		return memo3;
-	}
-	public void setMemo3(String memo3) {
-		this.memo3 = memo3;
-	}
-	public String getDay() {
-		return day;
-	}
-	public void setDay(String day) {
-		this.day = day;
-	}
-	public String getHtmlClass() {
-		return htmlClass;
-	}
-	public void setHtmlClass(String htmlClass) {
-		this.htmlClass = htmlClass;
-	}
 }
