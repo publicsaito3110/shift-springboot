4fb09cb575ac65ad7965d7eb7ef369e612b54a61
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-07 19:44:26 +0900

    fix: Interfaceの追加

--- src/main/java/com/shift/domain/Interface/HomeInterface.java
+++ src/main/java/com/shift/domain/Interface/HomeInterface.java
@@ -5,4 +5,8 @@ import org.springframework.web.servlet.ModelAndView;
 public interface HomeInterface extends BaseInterface {
 
 	void getDate(ModelAndView modelAndView);
+
+	void getNews(ModelAndView modelAndView);
+
+	void chengeDisplayNews(ModelAndView modelAndView);
 }
