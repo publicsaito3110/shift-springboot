52be53617fe7c013d1336693edecef47ccdb9428
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-07 19:49:22 +0900

    fix: Home機能の処理追加に伴う定数の追加

--- src/main/java/com/shift/common/Const.java
+++ src/main/java/com/shift/common/Const.java
@@ -10,4 +10,12 @@ public class Const {
 	//-----------
 	public static final String HTML_CLASS_CALENDAR_SAT = "calendar-sattday";
 	public static final String HTML_CLASS_CALENDAR_SUN = "calendar-sunday";
+
+
+	//-----
+	//Home
+	//-----
+	public static final int HOME_NEWS_SELECT_LIMIT = 5;
+	public static final int HOME_NEWS_DISPLAY_LIMIT_DATE = 14;
+	public static final String HOME_NEWS_NEW_ICON_SRC = "/png/icon/new-icon.png";
 }
