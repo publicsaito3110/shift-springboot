77e89e800dd517465d296fc4319396859888d69a
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-07 19:46:14 +0900

    fix: Home機能の処理追加に伴うServiceの処理の追加

--- src/main/java/com/shift/domain/service/HomeService.java
+++ src/main/java/com/shift/domain/service/HomeService.java
@@ -1,5 +1,22 @@
 package com.shift.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shift.domain.Interface.HomeInterface;
+
+@Component
 public class HomeService extends BaseService {
 
+	@Autowired
+	HomeInterface homeInterface;
+
+
+	public void home(ModelAndView modelAndView) {
+
+		homeInterface.getDate(modelAndView);
+		homeInterface.getNews(modelAndView);
+		homeInterface.chengeDisplayNews(modelAndView);
+	}
 }
