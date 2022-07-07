4d92f13010b4464dc4a570e5cbb7602b99959b5e
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-07 19:43:09 +0900

    fix: Homeの処理追加に伴うControllerの修正

--- src/main/java/com/shift/controller/HomeController.java
+++ src/main/java/com/shift/controller/HomeController.java
@@ -1,18 +1,23 @@
 package com.shift.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.shift.domain.service.HomeService;
+
 @Controller
 public class HomeController extends BaseController {
 
+	@Autowired
+	HomeService homeService;
 
 
 	@RequestMapping("/home")
 	public ModelAndView home(ModelAndView modelAndView) {
 
-//		dmService.dmMenue(modelAndView);
+		homeService.home(modelAndView);
 		modelAndView.setViewName("home");
 		return modelAndView;
 	}
