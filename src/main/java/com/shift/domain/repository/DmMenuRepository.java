d753d6b35cacffc469ba444fa864eeedeb9f3869
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-10 12:03:41 +0900

    rename: DmMenuDtoに修正

--- src/main/java/com/shift/domain/repository/DmMenuRepository.java
+++ src/main/java/com/shift/domain/repository/DmMenuRepository.java
@@ -6,15 +6,15 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.shift.domain.model.dto.DmMenueDto;
+import com.shift.domain.model.dto.DmMenuDto;
 
 /**
  * @author saito
  *
  */
 @Repository
-public interface DmMenueRepository extends BaseRepository<DmMenueDto, Integer>{
+public interface DmMenuRepository extends BaseRepository<DmMenuDto, Integer>{
 
 	@Query(value = "SELECT a.* FROM (SELECT DISTINCT d.id, d.msg, CASE WHEN d.send_user = :loginUser THEN s.name WHEN d.receive_user = :loginUser THEN u.name END AS msg_to_name, CASE WHEN d.send_user = :loginUser THEN s.id WHEN d.receive_user = :loginUser THEN u.id END AS msg_to_id FROM (dm d INNER JOIN USER u ON u.id = d.send_user) INNER JOIN USER s ON s.id = d.receive_user WHERE d.send_user = :loginUser OR d.receive_user = :loginUser ORDER BY d.id DESC) a GROUP BY a.msg_to_id", nativeQuery = true)
-	List<DmMenueDto> selectDmTalkHistoryByLoginUser(String loginUser);
+	List<DmMenuDto> selectDmTalkHistoryByLoginUser(String loginUser);
 }
