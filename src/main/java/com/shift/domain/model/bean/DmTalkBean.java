92f5d1c8cd68cfb868c0ded0480ce9c5bad2b748
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-10 12:16:16 +0900

    add: DmTalkBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/DmTalkBean.java
@@ -0,0 +1,21 @@
+package com.shift.domain.model.bean;
+
+import java.util.List;
+
+import com.shift.domain.model.entity.DmEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DmTalkBean {
+
+	private List<DmEntity> talkHistoryList;
+}
