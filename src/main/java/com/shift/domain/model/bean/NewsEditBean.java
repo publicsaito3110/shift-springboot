e470762bc332838229b8fd115045f787e39362a9
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-12 11:20:24 +0900

    add: NewsEditBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/NewsEditBean.java
@@ -0,0 +1,19 @@
+package com.shift.domain.model.bean;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewsEditBean {
+
+	private List<NewsBean> newsList;
+}
