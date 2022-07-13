487a0e18a2922c313610e0bf75c710b5a97e7342
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-13 20:49:44 +0900

    fix: 定数とNewsEditFormの追加に伴う処理の修正

--- src/main/java/com/shift/controller/NewsEditController.java
+++ src/main/java/com/shift/controller/NewsEditController.java
@@ -2,11 +2,14 @@ package com.shift.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.shift.common.Const;
 import com.shift.domain.model.bean.NewsEditBean;
 import com.shift.domain.service.NewsEditService;
+import com.shift.form.NewsEditForm;
 
 /**
  * @author saito
@@ -26,7 +29,23 @@ public class NewsEditController extends BaseController {
 		modelAndView.addObject("newsList", newsEditBean.getNewsList());
 		modelAndView.addObject("newsRecordList", newsEditBean.getNewsRecordList());
 
+		modelAndView.addObject("newsEditForm", new NewsEditForm());
+		modelAndView.addObject("newsCategoryArray", Const.NEWS_CATEGORY_ALL_ARRAY);
 		modelAndView.setViewName("news-edit");
+		return modelAndView;
+	}
+
+
+	@RequestMapping("/news-edit/test")
+	public ModelAndView newsEditTest(@ModelAttribute("sampleForm") NewsEditForm newsEditForm, ModelAndView modelAndView) {
+
+
+		modelAndView.addObject("ymd", newsEditForm.getYmd());
+		modelAndView.addObject("beforeTitle", newsEditForm.getBeforeTitle());
+		modelAndView.addObject("beforeCategory", newsEditForm.getBeforeCategory());
+		modelAndView.addObject("beforeContent", newsEditForm.getBeforeContent());
+
+		modelAndView.setViewName("/common/test");
 		return modelAndView;
 	}
 }
