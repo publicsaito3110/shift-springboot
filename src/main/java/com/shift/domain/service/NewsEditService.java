45ac80dc8c98843a601544e8a1e5e4cb8b28daaf
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-14 10:55:36 +0900

    fix: お知らせの修正機能を追加

--- src/main/java/com/shift/domain/service/NewsEditService.java
+++ src/main/java/com/shift/domain/service/NewsEditService.java
@@ -12,6 +12,7 @@ import com.shift.domain.model.bean.NewsBean;
 import com.shift.domain.model.bean.NewsEditBean;
 import com.shift.domain.model.entity.NewsEntity;
 import com.shift.domain.repository.NewsRepository;
+import com.shift.form.NewsEditForm;
 
 /**
  * @author saito
@@ -25,13 +26,32 @@ public class NewsEditService extends BaseService {
 	private HomeService homeService;
 
 	/**
-	 * [Service] (/news-edit)
+	 * [Service] (/news-edit)Get
 	 *
 	 * @param void
 	 * @return NewsEditBean
 	 */
 	public NewsEditBean newsEdit() {
 
+		HomeBean homeBean = homeService.home();
+		this.selectRecordNews();
+		List<NewsBean> newsRecordList = this.changeDisplayRecordNews();
+
+		//Beanにセット
+		NewsEditBean newsEditBean = new NewsEditBean(homeBean.getNewsList(), newsRecordList);
+		return newsEditBean;
+	}
+
+
+	/**
+	 * [Service] (/news-edit) Post
+	 *
+	 * @param NewsEditForm Request Param
+	 * @return NewsEditBean
+	 */
+	public NewsEditBean newsEditPost(NewsEditForm newsEditForm) {
+
+		this.updateRecordNews(newsEditForm);
 		HomeBean homeBean = homeService.home();
 		this.selectRecordNews();
 		List<NewsBean> newsRecordList = this.changeDisplayRecordNews();
@@ -78,5 +98,17 @@ public class NewsEditService extends BaseService {
 			newsRecordList.add(new NewsBean(newsEntity));
 		}
 		return newsRecordList;
+	}
+
+
+	private void updateRecordNews(NewsEditForm newsEditForm) {
+
+		NewsEntity newsEntity = new NewsEntity();
+		newsEntity.setId(newsEditForm.getId());
+		newsEntity.setYmd(newsEditForm.getYmd());
+		newsEntity.setTitle(newsEditForm.getTitle());
+		newsEntity.setCategory(newsEditForm.getCategory());
+		newsEntity.setContent(newsEditForm.getContent());
+		this.newsRepository.save(newsEntity);
 	}
 }
