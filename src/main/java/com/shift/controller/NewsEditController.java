170a236889cb84bc1f05b62785fe65286afa1311
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-14 10:51:43 +0900

    fix: Serviceクラスの変更に伴うControllerの修正

--- src/main/java/com/shift/controller/NewsEditController.java
+++ src/main/java/com/shift/controller/NewsEditController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.shift.common.Const;
@@ -22,7 +23,7 @@ public class NewsEditController extends BaseController {
 	NewsEditService newsEditService;
 
 
-	@RequestMapping("/news-edit")
+	@RequestMapping(value = "/news-edit", method = RequestMethod.GET)
 	public ModelAndView newsEdit(ModelAndView modelAndView) {
 
 		NewsEditBean newsEditBean = newsEditService.newsEdit();
@@ -30,22 +31,26 @@ public class NewsEditController extends BaseController {
 		modelAndView.addObject("newsRecordList", newsEditBean.getNewsRecordList());
 
 		modelAndView.addObject("newsEditForm", new NewsEditForm());
+		modelAndView.addObject("isModalResult", false);
 		modelAndView.addObject("newsCategoryArray", Const.NEWS_CATEGORY_ALL_ARRAY);
 		modelAndView.setViewName("news-edit");
 		return modelAndView;
 	}
 
 
-	@RequestMapping("/news-edit/test")
-	public ModelAndView newsEditTest(@ModelAttribute("sampleForm") NewsEditForm newsEditForm, ModelAndView modelAndView) {
+	@RequestMapping(value = "/news-edit", method = RequestMethod.POST)
+	public ModelAndView newsEditPost(@ModelAttribute NewsEditForm newsEditForm, ModelAndView modelAndView) {
 
+		NewsEditBean newsEditBean = newsEditService.newsEditPost(newsEditForm);
+		modelAndView.addObject("newsList", newsEditBean.getNewsList());
+		modelAndView.addObject("newsRecordList", newsEditBean.getNewsRecordList());
 
-		modelAndView.addObject("ymd", newsEditForm.getYmd());
-		modelAndView.addObject("beforeTitle", newsEditForm.getBeforeTitle());
-		modelAndView.addObject("beforeCategory", newsEditForm.getBeforeCategory());
-		modelAndView.addObject("beforeContent", newsEditForm.getBeforeContent());
-
-		modelAndView.setViewName("/common/test");
+		modelAndView.addObject("newsEditForm", new NewsEditForm());
+		modelAndView.addObject("isModalResult", true);
+		modelAndView.addObject("modalResultTitle", "お知らせ修正結果");
+		modelAndView.addObject("modalResultContent", "お知らせを修正しました。");
+		modelAndView.addObject("newsCategoryArray", Const.NEWS_CATEGORY_ALL_ARRAY);
+		modelAndView.setViewName("news-edit");
 		return modelAndView;
 	}
 }
