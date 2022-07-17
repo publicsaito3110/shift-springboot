b79fda9abc6b4d316febfda8dcaf25bfc4a62924
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-17 18:24:04 +0900

    add: AccountBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/AccountBean.java
@@ -0,0 +1,39 @@
+package com.shift.domain.model.bean;
+
+import com.shift.common.Const;
+import com.shift.domain.model.entity.UserEntity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+@NoArgsConstructor
+public class AccountBean {
+
+	//フィールド
+	private String userId;
+
+	private String name;
+
+	private String nameKana;
+
+	private boolean isAdminUser;
+
+
+	//コンストラクタ
+	public AccountBean(UserEntity userEntity) {
+		this.name = userEntity.getName();
+		this.nameKana = userEntity.getNameKana();
+
+		//管理者かどうかの判定
+		boolean isAdminUser = false;
+		if (Const.PATTERN_ADMIN_FLG.equals(userEntity.getAdminFlg())) {
+			isAdminUser = true;
+		}
+		this.isAdminUser = isAdminUser;
+	}
+}
