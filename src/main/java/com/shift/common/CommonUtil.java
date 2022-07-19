db97547273760eb86ddb60cc2c63d5b91658897c
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-20 08:56:31 +0900

    add: CommonUtilの追加

---
+++ src/main/java/com/shift/common/CommonUtil.java
@@ -0,0 +1,32 @@
+package com.shift.common;
+
+/**
+ * @author saito
+ *
+ */
+public class CommonUtil {
+	private CommonUtil() {
+		//インスタンス化を禁止
+	}
+
+
+	/**
+	 * 空文字変換処理
+	 *
+	 * <p>null のとき空文字に変換する<br>
+	 * ただし、null以外のときは何もしない
+	 * </p>
+	 *
+	 * @param value 全てのString型の変数
+	 * @return String nullのとき空文字に変換し、null以外のときは何もしない
+	 */
+	public static String changeEmptyByNull(String value) {
+
+		//nullでないとき
+		if (value != null) {
+			return value;
+		}
+
+		return "";
+	}
+}
