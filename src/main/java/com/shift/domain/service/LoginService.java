f13e62411a79a5dabcfed8861892d4bcea75e5bf
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-21 10:50:41 +0900

    fix: Constの変更に伴う修正

--- src/main/java/com/shift/domain/service/LoginService.java
+++ src/main/java/com/shift/domain/service/LoginService.java
@@ -91,7 +91,7 @@ public class LoginService extends BaseService {
 		}
 
 		//退職済みだったとき
-		if (Const.PATTERN_DEL_FLG.matches(this.userEntity.getDelFlg())) {
+		if (Const.PATTERN_USER_DEL_FLG.matches(this.userEntity.getDelFlg())) {
 
 			this.errorMassage = "このユーザーは現在ログインできません";
 			this.isLogin = false;
