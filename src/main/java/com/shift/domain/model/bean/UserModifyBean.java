47ef14cfeac871222760a6c8f80e2d03e512f2a1
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-24 19:03:19 +0900

    add: UserModifyBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/UserModifyBean.java
@@ -0,0 +1,19 @@
+package com.shift.domain.model.bean;
+
+import com.shift.domain.model.entity.UserEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModifyBean {
+
+	private UserEntity userEntity;
+}
