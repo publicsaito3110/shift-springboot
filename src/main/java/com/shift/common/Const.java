f243a9014077a7c2813a79315384728c1ba92977
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-24 19:00:06 +0900

    fix: 定数の追加

--- src/main/java/com/shift/common/Const.java
+++ src/main/java/com/shift/common/Const.java
@@ -47,6 +47,7 @@ public class Const {
 	public static final String USER_GENDER_1_MAN = "男性";
 	public static final String PATTERN_USER_GENDER_2_WOMEN = "2";
 	public static final String USER_GENDER_2_WOMEN = "女性";
+	public static final String[] USER_GENDER_ALL_ARRAY = {"男性", "女性"};
 
 
 	//-------
