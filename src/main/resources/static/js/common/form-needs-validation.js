fc08a75de99c4b9f9ad6e979a1250b6b709a76ac
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-27 15:43:47 +0900

    add: form-needs-validation.jsの追加

---
+++ src/main/resources/static/js/common/form-needs-validation.js
@@ -0,0 +1,21 @@
+$(function() {
+
+	//form内の入力値を全て取得
+	var forms = document.querySelectorAll(".needs-validation");
+
+	//formの入力値を全てチェックする
+	Array.prototype.slice.call(forms).forEach(function(form) {
+
+		//formが送信されたときの処理
+		form.addEventListener("submit", function(event) {
+
+			//未入力項目があるとき送信をキャンセルし、alertを表示
+			if (!form.checkValidity()) {
+				event.preventDefault();
+				event.stopPropagation();
+			}
+
+			form.classList.add("was-validated");
+		}, false)
+	})
+});
