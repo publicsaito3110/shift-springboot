bf49c91855390f968f78cc87004e34d786315032
publicsaito3110 <public.business.saito3110@gmail.com>
2022-07-28 16:37:16 +0900

    fix: お知らせ追加機能の追加に伴うControllerの修正

--- src/main/java/com/shift/controller/NewsEditController.java
+++ src/main/java/com/shift/controller/NewsEditController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.shift.common.Const;
@@ -29,6 +30,8 @@ public class NewsEditController extends BaseController {
 		NewsEditBean newsEditBean = newsEditService.newsEdit();
 		modelAndView.addObject("newsList", newsEditBean.getNewsList());
 		modelAndView.addObject("newsRecordList", newsEditBean.getNewsRecordList());
+		modelAndView.addObject("newsRecordableMinDate", newsEditBean.getNewsRecordableMinDate());
+		modelAndView.addObject("newsRecordableMaxDate", newsEditBean.getNewsRecordableMaxDate());
 
 		modelAndView.addObject("newsEditForm", new NewsEditForm());
 		modelAndView.addObject("isModalResult", false);
@@ -44,11 +47,32 @@ public class NewsEditController extends BaseController {
 		NewsEditBean newsEditBean = newsEditService.newsEditModify(newsEditForm);
 		modelAndView.addObject("newsList", newsEditBean.getNewsList());
 		modelAndView.addObject("newsRecordList", newsEditBean.getNewsRecordList());
+		modelAndView.addObject("newsRecordableMinDate", newsEditBean.getNewsRecordableMinDate());
+		modelAndView.addObject("newsRecordableMaxDate", newsEditBean.getNewsRecordableMaxDate());
 
 		modelAndView.addObject("newsEditForm", new NewsEditForm());
 		modelAndView.addObject("isModalResult", true);
 		modelAndView.addObject("modalResultTitle", "お知らせ修正結果");
 		modelAndView.addObject("modalResultContent", "お知らせを修正しました。");
+		modelAndView.addObject("newsCategoryArray", Const.NEWS_CATEGORY_ALL_ARRAY);
+		modelAndView.setViewName("news-edit");
+		return modelAndView;
+	}
+
+
+	@RequestMapping(value = "/news-edit/add", method = RequestMethod.POST)
+	public ModelAndView newsEditAdd(@RequestParam(value="title") String title, @RequestParam(value="date") String date, @RequestParam(value="category") String category, @RequestParam(value="content") String content, ModelAndView modelAndView) {
+
+		NewsEditBean newsEditBean = newsEditService.newsEditAdd(title, date, category, content);
+		modelAndView.addObject("newsList", newsEditBean.getNewsList());
+		modelAndView.addObject("newsRecordList", newsEditBean.getNewsRecordList());
+		modelAndView.addObject("newsRecordableMinDate", newsEditBean.getNewsRecordableMinDate());
+		modelAndView.addObject("newsRecordableMaxDate", newsEditBean.getNewsRecordableMaxDate());
+
+		modelAndView.addObject("newsEditForm", new NewsEditForm());
+		modelAndView.addObject("isModalResult", true);
+		modelAndView.addObject("modalResultTitle", "お知らせ新規追加結果");
+		modelAndView.addObject("modalResultContent", "お知らせを新規追加しました。");
 		modelAndView.addObject("newsCategoryArray", Const.NEWS_CATEGORY_ALL_ARRAY);
 		modelAndView.setViewName("news-edit");
 		return modelAndView;
