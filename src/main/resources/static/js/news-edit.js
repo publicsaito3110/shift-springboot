e16fa588f159b247310e4cbac0472c559b109775
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-01 11:02:24 +0900

    fix: news-edit.htmlの変更に伴うjsの修正

--- src/main/resources/static/js/news-edit.js
+++ src/main/resources/static/js/news-edit.js
@@ -7,10 +7,14 @@ $(function() {
 
 		//お知らせの日付, タイトル, 内容をモーダル1にセット
 		$("#modal1-title").text($(this).children(".news-title").text());
-		$("#modal1-date").html($(this).children(".news-date").text());
+		$("#modal1-date").text($(this).children(".news-date").text());
 		$("#modal1-content").html($(this).children(".news-content-display").val());
 
-		//お知らせ修正前の値を取得し、モーダル2にセット
+		//お知らせの日付, タイトル, 内容, 修正前の値を取得し、モーダル2にセット
+		$("#modal2-news-date").text($(this).children(".news-date").text());
+		$("#modal2-news-title").val($(this).children(".news-title").text());
+		$("#modal2-news-category").val($(this).children(".news-category").val());
+		$("#modal2-news-content").html($(this).children(".news-content-display").val());
 		$("#modal2-news-id").val($(this).children(".news-id").val());
 		$("#modal2-news-ymd").val($(this).children(".news-ymd").val());
 	});
