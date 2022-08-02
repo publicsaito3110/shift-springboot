6c3129650fcc1ee97dc82a31748c5e653483830d
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-02 17:21:38 +0900

    add: dm.jsの追加

---
+++ src/main/resources/static/js/dm.js
@@ -0,0 +1,21 @@
+$(function(){
+
+	//------------------------
+	// 検索ボタン押下時の処理
+	//------------------------
+	$("#user-address-seach-btn").on("click", function (){
+
+		//非同期通信
+		$.ajax({
+		url: "/dm/talk/address",
+		type: "POST",
+		data: {keyword : $("#user-address-seach-keyword").val()}
+		}).done(function (result) {
+			// 通信成功時のコールバック
+			$("#user-address-box").html(result);
+		}).fail(function () {
+			// 通信失敗時のコールバック
+			alert("読み込みに失敗しました。");
+		});
+	});
+});
