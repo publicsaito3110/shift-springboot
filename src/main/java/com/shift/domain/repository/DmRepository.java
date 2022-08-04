fcbb55b19ce5f434a7d4828ba6309cb0be2f5645
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-04 09:58:24 +0900

    fix: DmChatRepositoryの追加に伴うRepositoryの修正

--- src/main/java/com/shift/domain/repository/DmRepository.java
+++ src/main/java/com/shift/domain/repository/DmRepository.java
@@ -1,9 +1,6 @@
 package com.shift.domain.repository;
 
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.shift.domain.model.entity.DmEntity;
@@ -15,6 +12,4 @@ import com.shift.domain.model.entity.DmEntity;
 @Repository
 public interface DmRepository extends BaseRepository<DmEntity, Integer>{
 
-@Query(value = "SELECT d.id, d.msg_date, d.msg, CASE WHEN d.send_user = :loginUser THEN 'login-user' WHEN d.send_user != :loginUser THEN 'not-login-user' END AS send_user, d.receive_user FROM dm d WHERE d.send_user = :loginUser AND d.receive_user = :receiveUser OR d.send_user = :receiveUser AND d.receive_user = :loginUser ORDER BY d.id", nativeQuery = true)
-	public List<DmEntity> selectTalkHistoryByLoginUserReceiveUser(String loginUser, String receiveUser);
 }
