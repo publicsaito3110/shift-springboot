b7addb6f61f4470b3551a2f1efb6bd02b2a87eaf
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-04 09:55:33 +0900

    add: DmChatDtoの追加

---
+++ src/main/java/com/shift/domain/model/dto/DmChatDto.java
@@ -0,0 +1,48 @@
+package com.shift.domain.model.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.shift.common.CommonLogic;
+
+import lombok.Data;
+
+/**
+ * @author saito
+ *
+ */
+@Entity
+@Data
+public class DmChatDto {
+
+
+	//フィールド
+	@Id
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "msg")
+	private String msg;
+
+	@Column(name = "msg_date")
+	private String msgDate;
+
+	@Column(name = "html_class_send_user", unique = true)
+	private String htmlClassSendUser;
+
+
+	//メソッド
+	public String getMsgDateFormatDate() {
+
+		//フォーマットをMM/DD hh:mmに変換する
+		String date = this.msgDate.substring(5, 16);
+		return date.replaceAll("-", "/");
+	}
+
+	public String getMsgAfterBreakLine() {
+
+		//改行対応
+		return new CommonLogic().changeAfterbreakLine(this.msg);
+	}
+}
