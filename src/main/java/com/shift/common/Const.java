23f53277567b78c979927a5ac0dad9dbb1d3377f
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-08 08:38:42 +0900

    fix: 定数の追加

--- src/main/java/com/shift/common/Const.java
+++ src/main/java/com/shift/common/Const.java
@@ -57,6 +57,8 @@ public class Const {
 	public static final String SESSION_KEYWORD_ACCOUNT_BEAN = "KEY1_SESSION_ACCOUNT_BEAN";
 
 	//バリデーションパターン
+	public static final String PATTERN_NEWS_YMD_ALL = "^[0-9]{4}+(0[1-9]|1[0-2])+(0[1-9]|[12][0-9]|3[01])$";
+
 	public static final String PATTERN_USER_ADMIN_FLG = "1";
 	public static final String PATTERN_USER_ADMIN_FLG_ALL = "1|^$";
 	public static final String PATTERN_USER_DEL_FLG = "1";
