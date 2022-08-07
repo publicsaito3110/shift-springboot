52f37e63f54e4536230367196348733113d8631c
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-08 08:35:58 +0900

    fix: NewsEditModifyFormをバリデーション対応

---
+++ src/main/java/com/shift/form/NewsEditModifyForm.java
@@ -0,0 +1,35 @@
+package com.shift.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Range;
+
+import com.shift.common.Const;
+
+import lombok.Data;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+public class NewsEditModifyForm {
+
+	@NotBlank(message = "必須入力です")
+	private String id;
+
+	@NotBlank(message = "必須入力です")
+	@Pattern(regexp = Const.PATTERN_NEWS_YMD_ALL, message = "入力値が不正です")
+	private String ymd;
+
+	@NotBlank(message = "必須入力です")
+	private String title;
+
+	@NotBlank(message = "必須入力です")
+	@Range(min = 1, max = 3, message = "入力値が不正です")
+	private String category;
+
+	@NotBlank(message = "必須入力です")
+	private String content;
+}
