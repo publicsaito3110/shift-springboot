f7056192b10689160cc356667dee41ca31bc330d
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-07 22:02:12 +0900

    fix: Formにバリデーションを追加

--- src/main/java/com/shift/form/UserAddForm.java
+++ src/main/java/com/shift/form/UserAddForm.java
@@ -1,5 +1,10 @@
 package com.shift.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import com.shift.common.Const;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,14 +18,35 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserAddForm {
 
+	@NotBlank(message = "必須入力です")
+	@Pattern(regexp = Const.PATTERN_USER_ID_ALL, message = "半角英数字で入力してください")
 	private String userId;
+
+	@NotBlank(message = "必須入力です")
 	private String name;
+
+	@NotBlank(message = "必須入力です")
+	@Pattern(regexp = Const.PATTERN_USER_NAME_KANA_ALL, message = "カタカナで入力してください")
 	private String nameKana;
+
+	@NotBlank(message = "必須入力です")
+	@Pattern(regexp = Const.PATTERN_USER_GENDER_ALL, message = "入力値が不正です")
 	private String gender;
+
+	@NotBlank(message = "必須入力です")
+	@Pattern(regexp = Const.PATTERN_USER_PASSWORD_ALL, message = "半角英数字で入力してください")
 	private String password;
+
 	private String address;
+
+	@Pattern(regexp = Const.PATTERN_USER_TEL_ALL, message = "- (ハイフンなし)8-9桁で入力してください")
 	private String tel;
+
+	@Pattern(regexp = Const.PATTERN_USER_EMAIL_ALL, message = "不正なメールアドレスです")
 	private String email;
+
 	private String note;
+
+	@Pattern(regexp = Const.PATTERN_USER_ADMIN_FLG_ALL, message = "入力値が不正です")
 	private String adminFlg;
 }
