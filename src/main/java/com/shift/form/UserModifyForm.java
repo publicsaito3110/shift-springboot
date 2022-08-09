9b7a736293c5feec0084eee5d665f5daf0747dbe
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-09 12:28:28 +0900

    fix: バリデーション対応に伴うFormの修正

--- src/main/java/com/shift/form/UserModifyForm.java
+++ src/main/java/com/shift/form/UserModifyForm.java
@@ -1,21 +1,33 @@
 package com.shift.form;
 
-import lombok.AllArgsConstructor;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import com.shift.common.Const;
+
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * @author saito
  *
  */
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserModifyForm {
 
+	@NotBlank(message = "必須入力です")
+	@Pattern(regexp = Const.PATTERN_USER_ID_ALL, message = "入力値が不正です")
 	private String userId;
+
+	@NotBlank(message = "必須入力です")
 	private String name;
+
+	@NotBlank(message = "必須入力です")
+	@Pattern(regexp = Const.PATTERN_USER_NAME_KANA_ALL, message = "カタカナで入力してください")
 	private String nameKana;
+
+	@NotBlank(message = "必須入力です")
+	@Pattern(regexp = Const.PATTERN_USER_GENDER_ALL, message = "入力値が不正です")
 	private String gender;
+
 	private String note;
 }
