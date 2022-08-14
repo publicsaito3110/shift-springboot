946b63dd69ab301edd2ddb696d46764faa555e9b
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-14 22:10:30 +0900

    add: UserDetailsServiceImplの追加

---
+++ src/main/java/com/shift/domain/service/UserDetailsServiceImpl.java
@@ -0,0 +1,52 @@
+package com.shift.domain.service;
+
+import java.util.HashSet;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.shift.domain.model.entity.UserEntity;
+import com.shift.domain.repository.UserRepository;
+
+/**
+ * @author saito
+ *
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		//usernameを検索
+		Optional<UserEntity> userEntityOpt = userRepository.findById(username);
+
+		//usernameが存在しないとき
+		if (!userEntityOpt.isPresent()) {
+			throw new UsernameNotFoundException(username + " is not found");
+		}
+
+		//---------------------
+		// UserDetailsを返却
+		//---------------------
+
+		//UserEntityで取得
+		UserEntity userEntity = userEntityOpt.get();
+
+		//User(UserDetailsインターフェースの実装クラス)にセットして返却
+		var grantedAuthorities = new HashSet<GrantedAuthority>();
+		grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + userEntity.getAdminFlgFormatRole()));
+
+		return new User(userEntity.getId(), userEntity.getPassword(), grantedAuthorities);
+	}
+}
