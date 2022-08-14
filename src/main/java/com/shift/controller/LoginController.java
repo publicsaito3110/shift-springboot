011c2b39845cfb78cb4329d8ef008543fb710833
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-14 22:13:17 +0900

    fix: UserDetailsServiceImpl追加に伴うController修正

--- src/main/java/com/shift/controller/LoginController.java
+++ src/main/java/com/shift/controller/LoginController.java
@@ -1,13 +1,12 @@
 package com.shift.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.shift.domain.model.bean.LoginBean;
+import com.shift.domain.model.bean.LoginAuthBean;
 import com.shift.domain.service.LoginService;
 
 /**
@@ -30,18 +29,22 @@ public class LoginController extends BaseController {
 	}
 
 
-	@RequestMapping(value = "/login/auth", method = RequestMethod.POST)
-	public ModelAndView loginAuth(@RequestParam(value="userId") String userId, @RequestParam(value="password") String password, ModelAndView modelAndView) {
+	@RequestMapping("/login/auth")
+	public ModelAndView loginAuth(Authentication authentication, ModelAndView modelAndView) {
 
-		LoginBean loginBean = this.loginService.loginAuth(userId, password);
+		//--------------------------
+		// ログイン認証成功時に実行
+		//--------------------------
+		LoginAuthBean loginAuthBean = this.loginService.loginAuth(authentication.getName());
 
-		//ログインができなかったときログイン画面へ戻す
-		if (!loginBean.isLogin()) {
+		//ログイン可能ユーザーでなかったとき
+		if (!loginAuthBean.isLogin()) {
 
+			//ログイン画面へ戻す
 			modelAndView.addObject("isAlertLoginFailed", true);
-			modelAndView.addObject("errorMassage", loginBean.getErrorMassage());
+			modelAndView.addObject("errorMassage", loginAuthBean.getErrorMassage());
 
-			modelAndView.setViewName("login");
+			modelAndView.setViewName("/login");
 			return modelAndView;
 		}
 
