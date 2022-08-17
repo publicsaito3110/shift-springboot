800c0f719f116314314555a0a8e571ee2d0dc308
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-17 11:20:35 +0900

    fix: バリデーション判定メソッドの追加

--- src/main/java/com/shift/common/CommonUtil.java
+++ src/main/java/com/shift/common/CommonUtil.java
@@ -17,7 +17,7 @@ public class CommonUtil {
 	 * ただし、null以外のときは何もしない
 	 * </p>
 	 *
-	 * @param value 全てのString型の変数
+	 * @param value 全てのStringの値
 	 * @return String nullのとき空文字に変換し、null以外のときは何もしない
 	 */
 	public static String changeEmptyByNull(String value) {
@@ -28,5 +28,39 @@ public class CommonUtil {
 		}
 
 		return value;
+	}
+
+
+	/**
+	 * バリデーション判定処理
+	 *
+	 * <p>バリデーションを正規表現で判定する<br>
+	 * そして、バリデーションの結果をbooleanで返す
+	 * </p>
+	 *
+	 * @param value 全てのStringの値<br>
+	 * ただし、nullのときは必ずバリデーションが失敗する
+	 * @param regex 正規表現<br>
+	 * ただし、nullまたは正規表現に則していないのときは必ずバリデーションが失敗する
+	 * @return boolean<br>
+	 * true: バリデーションが成功(正規表現に基づいている)<br>
+	 * false: バリデーションが失敗(正規表現に基づいていないまたは引数が異常なとき)
+	 */
+	public static boolean isSuccessValidation(String value, String regex) {
+
+		try {
+
+			//バリデーションが成功したとき
+			if (value.matches(regex)) {
+				return true;
+			}
+
+		} catch (Exception e) {
+
+			//例外発生時、falseを返す
+			return false;
+		}
+
+		return false;
 	}
 }
