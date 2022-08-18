2d2571c0a08848d2fc3a3024bf5f093f39d52134
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-18 18:00:58 +0900

    fix: フィールド及びメソッドの追加

--- src/main/java/com/shift/domain/model/bean/ValidationBean.java
+++ src/main/java/com/shift/domain/model/bean/ValidationBean.java
@@ -1,5 +1,7 @@
 package com.shift.domain.model.bean;
 
+import com.shift.common.CommonUtil;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +15,37 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ValidationBean {
 
+	private String inputQuery;
+
 	private boolean isValidationSuccess;
 
 	private String errorMessage;
+
+
+	//メソッド
+	public String inputQueryFormatHtml() {
+		return CommonUtil.changeEmptyByNull(this.inputQuery);
+	}
+
+	public String htmlClassValid() {
+
+		//バリデーションチェックを行っていないとき
+		if (this.errorMessage == null) {
+			return "";
+		}
+
+		//バリデーションチェック成功済みのとき
+		if (this.isValidationSuccess) {
+
+			//Bootstrapのvalid(class)を返す
+			return "is-valid ";
+		}
+
+		//Bootstrapのinvalid(class)を返す
+		return "is-invalid ";
+	}
+
+	public String errorMessageFormatHtml() {
+		return CommonUtil.changeEmptyByNull(this.errorMessage);
+	}
 }
