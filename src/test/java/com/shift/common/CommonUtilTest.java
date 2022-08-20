11276bb707e79dc0c84af8a166fcc07ed9edaf4d
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-20 20:00:01 +0900

    fix: テストケースisSuccessValidationTestの追加

--- src/test/java/com/shift/common/CommonUtilTest.java
+++ src/test/java/com/shift/common/CommonUtilTest.java
@@ -30,6 +30,8 @@ class CommonUtilTest {
 		//-----------------------------------------------------------------
 		// 実行
 		//-----------------------------------------------------------------
+
+		//テスト(正常値)
 		String resultN1 = CommonUtil.changeEmptyByNull(valueN1);
 		String resultN2 = CommonUtil.changeEmptyByNull(valueN2);
 		String resultN3 = CommonUtil.changeEmptyByNull(valueN3);
@@ -37,8 +39,64 @@ class CommonUtilTest {
 		//-----------------------------------------------------------------
 		// 検証
 		//-----------------------------------------------------------------
+
+		//結果(正常値)
 		assertEquals("", resultN1);
 		assertEquals("", resultN2);
 		assertEquals("value", resultN3);
+	}
+
+
+	@Test
+	@DisplayName("isSuccessValidationTest: バリデーション判定処理")
+	public void isSuccessValidationTest() {
+
+		//-----------------------------------------------------------------
+		// 準備
+		//-----------------------------------------------------------------
+
+		//テストケース(正常値)
+		String valueN1 = "a";
+		String regexN1 = "a|b";
+		String valueN2 = "a";
+		String regexN2 = "1|2";
+		String valueN3 = "";
+		String regexN3 = "1|2";
+
+		//テストケース(異常値)
+		String valueUN1 = null;
+		String regexUN1 = "a|b";
+		String valueUN2 = "a";
+		String regexUN2 = null;
+		String valueUN3 = "a";
+		String regexUN3 = "<";
+
+		//-----------------------------------------------------------------
+		// 実行
+		//-----------------------------------------------------------------
+
+		//テスト(正常値)
+		boolean resultN1 = CommonUtil.isSuccessValidation(valueN1, regexN1);
+		boolean resultN2 = CommonUtil.isSuccessValidation(valueN2, regexN2);
+		boolean resultN3 = CommonUtil.isSuccessValidation(valueN3, regexN3);
+
+		//テスト(異常値)
+		boolean resultUN1 = CommonUtil.isSuccessValidation(valueUN1, regexUN1);
+		boolean resultUN2 = CommonUtil.isSuccessValidation(valueUN2, regexUN2);
+		boolean resultUN3 = CommonUtil.isSuccessValidation(valueUN3, regexUN3);
+
+		//-----------------------------------------------------------------
+		// 検証
+		//-----------------------------------------------------------------
+
+		//結果(正常値)
+		assertEquals(true, resultN1);
+		assertEquals(false, resultN2);
+		assertEquals(false, resultN3);
+
+		//結果(異常値)
+		assertEquals(false, resultUN1);
+		assertEquals(false, resultUN2);
+		assertEquals(false, resultUN3);
 	}
 }
