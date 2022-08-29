f535a63fcc4aeb445c7f6e5d9d07bfb906f018ef
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-29 14:51:21 +0900

    fix: Authentication(認証)に関する共通処理の追加

--- src/main/java/com/shift/common/CommonUtil.java
+++ src/main/java/com/shift/common/CommonUtil.java
@@ -1,5 +1,7 @@
 package com.shift.common;
 
+import org.springframework.security.core.Authentication;
+
 /**
  * @author saito
  *
@@ -13,7 +15,7 @@ public class CommonUtil {
 	/**
 	 * null空文字変換処理
 	 *
-	 * <p>null のとき空文字に変換する<br>
+	 * <p>nullのとき空文字に変換する<br>
 	 * ただし、null以外のときは何もしない
 	 * </p>
 	 *
@@ -28,6 +30,42 @@ public class CommonUtil {
 		}
 
 		return value;
+	}
+
+
+	/**
+	 * ユーザROLE取得処理
+	 *
+	 * <p>authenticationからROLEを取得し、配列で返す<br>
+	 * また、役職が複数ある場合は役職ごとに格納される<br>
+	 * ただし、authenticationからROLEが取得できないときはnullを返す
+	 * </p>
+	 *
+	 * @param authentication Controllerから取得したAuthentication
+	 * @return String[] ROLE(役職が複数あるときは要素数が2以上になる)
+	 */
+	public static String[] getUserRoleArrayByAuthentication(Authentication authentication) {
+
+		//authenticationがnullのとき
+		if (authentication == null) {
+			return null;
+		}
+
+		//authenticationからROLEを取得
+		String userRole = authentication.getAuthorities().toString();
+
+		//userRoleがnullのとき
+		if (userRole == null) {
+			return null;
+		}
+
+		//[]を削除し、ROLE_...のフォーマットにする
+		String trim1UserRole = userRole.replaceAll("\\[", "");
+		String trim2UserRole = trim1UserRole.replaceAll("\\]", "");
+
+		//役職ごとに配列に変換し、返す
+		String[] userRoleArray = trim2UserRole.split(",");
+		return userRoleArray;
 	}
 
 
