ed8a13a2b1d685e925041dafd573b1ec4783c764
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-04 16:01:58 +0900

    add: SchedulePreRepositoryの追加

---
+++ src/main/java/com/shift/domain/repository/SchedulePreRepository.java
@@ -0,0 +1,30 @@
+package com.shift.domain.repository;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.shift.domain.model.entity.SchedulePreEntity;
+
+/**
+ * @author saito
+ *
+ */
+@Repository
+public interface SchedulePreRepository extends BaseRepository<SchedulePreEntity, Integer> {
+
+	/**
+	 * [DB]スケジュール検索処理
+	 *
+	 * <p>現在の年月からユーザーの1ヵ月分のスケジュール予定を取得する<br>
+	 * ただし、登録済みのスケジュールがないときはEmptyとなる<br>
+	 * なた、日付が存在しない日(2月 -> 30, 31日etc)は登録されない
+	 * </p>
+	 *
+	 * @param ym 現在の年月(YYYYMM)
+	 * @param loginUser ログインしているユーザー
+	 * @return SchedulePreEntity<br>
+	 * フィールド(SchedulePreEntity)<br>
+	 * id, ym, user, 1, 2, 3, 4, 5... 30, 31
+	 */
+	public SchedulePreEntity findByYmUser(String ym, String loginUser);
+}
