e095d574025b957559a5041a2c5689d04a35be8b
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-05 17:35:58 +0900

    add: ScheduleCalendarBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/ScheduleCalendarBean.java
@@ -0,0 +1,18 @@
+package com.shift.domain.model.bean;
+
+import lombok.Data;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+public class ScheduleCalendarBean {
+
+	//フィールド
+	private String day;
+
+	private String schedule;
+
+	private String htmlClass;
+}
