e58ef4c9f6b2c7dbb6b15ac27e15080246fe6d8c
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-06 20:06:44 +0900

    fix: ScheduleTimeRepositoryの追加

---
+++ src/main/java/com/shift/domain/repository/ScheduleTimeRepository.java
@@ -0,0 +1,15 @@
+package com.shift.domain.repository;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.shift.domain.model.entity.ScheduleTimeEntity;
+
+/**
+ * @author saito
+ *
+ */
+@Repository
+public interface ScheduleTimeRepository extends BaseRepository<ScheduleTimeEntity, String> {
+
+}
