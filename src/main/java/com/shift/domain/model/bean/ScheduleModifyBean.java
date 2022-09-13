80da23a01239269b92863a3d30433459945e8c1e
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-13 21:14:49 +0900

    fix: ScheduleModifyBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/ScheduleModifyBean.java
@@ -0,0 +1,32 @@
+package com.shift.domain.model.bean;
+
+import java.util.List;
+
+import com.shift.domain.model.entity.ScheduleTimeEntity;
+
+import lombok.Data;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+public class ScheduleModifyBean {
+
+	//フィールド
+	private int year;
+
+	private int month;
+
+	private List<Boolean[]> isScheduleRecordedArrayList;
+
+	private List<Integer> calendarList;
+
+	private String nowYm;
+
+	private String afterYm;
+
+	private String beforeYm;
+
+	private List<ScheduleTimeEntity> scheduleTimeList;
+}
