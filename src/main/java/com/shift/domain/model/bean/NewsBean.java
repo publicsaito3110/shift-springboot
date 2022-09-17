40ed4b7ea5a840c0df3963e9ffcbb86b7642c370
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-17 18:52:03 +0900

    fix: メソッド及びフィールドの修正

--- src/main/java/com/shift/domain/model/bean/NewsBean.java
+++ src/main/java/com/shift/domain/model/bean/NewsBean.java
@@ -17,9 +17,13 @@ public class NewsBean {
 
 	//フィールド
 	private String id;
+
 	private String ymd;
+
 	private String category;
+
 	private String title;
+
 	private String content;
 
 	private String srcPngNewIcon;
@@ -27,11 +31,11 @@ public class NewsBean {
 
 	//コンストラクタ
 	public NewsBean(NewsEntity newsEntity) {
-		this.id = newsEntity.getId();
-		this.ymd = newsEntity.getYmd();
-		this.category = newsEntity.getCategory();
-		this.title = newsEntity.getTitle();
-		this.content = newsEntity.getContent();
+		id = newsEntity.getId();
+		ymd = newsEntity.getYmd();
+		category = newsEntity.getCategory();
+		title = newsEntity.getTitle();
+		content = newsEntity.getContent();
 	}
 
 
@@ -39,21 +43,20 @@ public class NewsBean {
 	public String ymdFormatDate() {
 
 		//日付形式(mm/dd)返す
-		return this.ymd.substring(4, 6) + "/" + this.ymd.substring(6, 8);
+		return ymd.substring(4, 6) + "/" + ymd.substring(6, 8);
 	}
 
 
 	public String categoryFormatPngSrc() {
 
 		//category-icon.pngのsrcを返す
-		return Const.HOME_NEWS_CATEGORY_ICON_SRC +  this.category + ".png";
+		return Const.HOME_NEWS_CATEGORY_ICON_SRC +  category + ".png";
 	}
 
 
 	public String contentAfterBreakLine() {
 
 		//改行対応
-		CommonLogic commonLogic = new CommonLogic();
-		return commonLogic.changeAfterBreakLine(this.content);
+		return new CommonLogic().changeAfterBreakLine(content);
 	}
 }
