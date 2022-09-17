186c423fba5feb74e994d96afb8c3637e7ec98fa
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-17 19:38:50 +0900

    fix: ユーザー情報取得処理の追加

--- src/main/java/com/shift/domain/service/HomeService.java
+++ src/main/java/com/shift/domain/service/HomeService.java
@@ -1,10 +1,14 @@
 package com.shift.domain.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shift.domain.model.bean.CmnNewsBean;
 import com.shift.domain.model.bean.HomeBean;
+import com.shift.domain.model.entity.UserEntity;
+import com.shift.domain.repository.UserRepository;
 import com.shift.domain.service.common.CmnNewsService;
 
 /**
@@ -15,22 +19,52 @@ import com.shift.domain.service.common.CmnNewsService;
 public class HomeService extends BaseService {
 
 	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
 	private CmnNewsService cmnNewsService;
 
 
 	/**
 	 * [Service] (/home)
 	 *
-	 * @param void
+	 * @param loginUser Authenticationから取得したユーザID
 	 * @return HomeBean
 	 */
-	public HomeBean home() {
+	public HomeBean home(String loginUser) {
 
+		UserEntity userEntity = selectUserByLoginUser(loginUser);
 		//CmnNewsService(共通サービス)から処理結果を取得
 		CmnNewsBean cmnNewsBean = cmnNewsService.generateDisplayNowNews();
 
 		//Beanにセット
-		HomeBean homeBean = new HomeBean(cmnNewsBean.getNewsList());
+		HomeBean homeBean = new HomeBean(userEntity, cmnNewsBean.getNewsList());
 		return homeBean;
+	}
+
+
+	/**
+	 * [DB]ユーザ検索処理
+	 *
+	 * <p>loginUserと一致するユーザを取得する<br>
+	 * ただし、一致するユーザーがいない場合はnullとなる
+	 * </p>
+	 *
+	 * @param loginUser Authenticationから取得したユーザID
+	 * @return UserEntity<br>
+	 * フィールド(UserEntity)<br>
+	 * id, name, nameKana, gender, password, address, tel, email, note, admin_flg, del_flg
+	 */
+	private UserEntity selectUserByLoginUser(String loginUser) {
+
+		Optional<UserEntity> userEntityOpt = userRepository.findById(loginUser);
+
+		//ないとき
+		if (!userEntityOpt.isPresent()) {
+			return null;
+		}
+
+		//UserEntityを返す
+		return userEntityOpt.get();
 	}
 }
