d6b726019c5959e131f27515bb819cea3ce19a3d
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-20 14:23:24 +0900

    add: CmnScheduleBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/CmnScheduleBean.java
@@ -0,0 +1,29 @@
+package com.shift.domain.model.bean;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CmnScheduleBean {
+
+	private int year;
+
+	private int month;
+
+	private List<Integer> calendarList;
+
+	private String nowYm;
+
+	private String nextYm;
+
+	private String beforeYm;
+}
