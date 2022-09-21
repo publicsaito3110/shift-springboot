cc0eb34f35a29fab2c24a0b66f9209c38cbd9b2a
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-21 20:33:04 +0900

    add: ScheduleDecisionBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/ScheduleDecisionBean.java
@@ -0,0 +1,28 @@
+package com.shift.domain.model.bean;
+
+import java.util.List;
+
+import com.shift.domain.model.entity.ScheduleTimeEntity;
+
+import lombok.Data;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+public class ScheduleDecisionBean {
+
+	//フィールド
+	private int year;
+
+	private int month;
+
+	private List<Integer> calendarList;
+
+	private String afterYm;
+
+	private String beforeYm;
+
+	private List<ScheduleTimeEntity> scheduleTimeList;
+}
