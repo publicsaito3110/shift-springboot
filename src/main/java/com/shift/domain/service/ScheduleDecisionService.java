5de69fc31a9e3fa1ad2ba1b47c83bf2219ee96d9
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-22 20:51:37 +0900

    fix: 確定スケジュール取得処理追加Serviceの修正

--- src/main/java/com/shift/domain/service/ScheduleDecisionService.java
+++ src/main/java/com/shift/domain/service/ScheduleDecisionService.java
@@ -1,14 +1,19 @@
 package com.shift.domain.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shift.common.Const;
 import com.shift.domain.model.bean.CmnScheduleBean;
 import com.shift.domain.model.bean.ScheduleDecisionBean;
+import com.shift.domain.model.bean.ScheduleDecisionModifyBean;
+import com.shift.domain.model.dto.ScheduleUserDto;
 import com.shift.domain.model.entity.ScheduleTimeEntity;
 import com.shift.domain.repository.ScheduleTimeRepository;
+import com.shift.domain.repository.ScheduleUserRepository;
 import com.shift.domain.service.common.CmnScheduleService;
 
 /**
@@ -17,6 +22,9 @@ import com.shift.domain.service.common.CmnScheduleService;
  */
 @Service
 public class ScheduleDecisionService extends BaseService {
+
+	@Autowired
+	private ScheduleUserRepository scheduleUserRepository;
 
 	@Autowired
 	private ScheduleTimeRepository scheduleTimeRepository;
@@ -29,10 +37,9 @@ public class ScheduleDecisionService extends BaseService {
 	 * [Service] (/schedule-decision)
 	 *
 	 * @param ym RequestParameter
-	 * @param loginUser Authenticationから取得したユーザID
 	 * @return ScheduleDecisionBean
 	 */
-	public ScheduleDecisionBean scheduleDecision(String ym, String loginUser) {
+	public ScheduleDecisionBean scheduleDecision(String ym) {
 
 		//CmnScheduleService(共通サービス)から処理結果を取得
 		CmnScheduleBean cmnScheduleBean = cmnScheduleService.generateCalendarYmByYm(ym);
@@ -44,6 +51,7 @@ public class ScheduleDecisionService extends BaseService {
 		scheduleDecisionBean.setYear(cmnScheduleBean.getYear());
 		scheduleDecisionBean.setMonth(cmnScheduleBean.getMonth());
 		scheduleDecisionBean.setCalendarList(cmnScheduleBean.getCalendarList());
+		scheduleDecisionBean.setNowYm(cmnScheduleBean.getNowYm());
 		scheduleDecisionBean.setAfterYm(cmnScheduleBean.getNextYm());
 		scheduleDecisionBean.setBeforeYm(cmnScheduleBean.getBeforeYm());
 		scheduleDecisionBean.setScheduleTimeList(scheduleTimeList);
@@ -52,10 +60,29 @@ public class ScheduleDecisionService extends BaseService {
 
 
 	/**
+	 * [Service] (/schedule-decision/modify)
+	 *
+	 * @param day RequestParameter
+	 * @return ScheduleDecisionBean
+	 */
+	public ScheduleDecisionModifyBean scheduleDecisionModify(String day) {
+
+		//Service
+		List<ScheduleUserDto> scheduleUserList = selectScheduleDay("202209", day);
+		List<ScheduleTimeEntity> scheduleTimeList = selectScheduleTime();
+
+		//Beanにセット
+		ScheduleDecisionModifyBean scheduleDecisionModifyBean = new ScheduleDecisionModifyBean(scheduleUserList, scheduleTimeList);
+		return scheduleDecisionModifyBean;
+	}
+
+
+	/**
 	 * [DB]シフト時間取得処理
 	 *
 	 * <p>管理者が設定したシフト時間を取得する</p>
 	 *
+	 * @param void
 	 * @return List<ScheduleTimeEntity> <br>
 	 * フィールド(List&lt;ScheduleTimeEntity&gt;)<br>
 	 * id, name, startHms, endHms, restHms
@@ -65,5 +92,38 @@ public class ScheduleDecisionService extends BaseService {
 
 		List<ScheduleTimeEntity> scheduleTimeEntityList = scheduleTimeRepository.findAll();
 		return scheduleTimeEntityList;
+	}
+
+
+	/**
+	 * [DB]確定スケジュール取得処理
+	 *
+	 * <p>ym及びdayに該当する確定スケジュールをユーザごとに取得する<br>
+	 * ym=200001,day=1のとき2000年1月1日, ym=200001,day=2のとき2000年1月2日...<br>
+	 * ただし、登録済みのスケジュールが1つもないまたは存在しない日付のときはEmptyとなる
+	 * </p>
+	 *
+	 * @param ym 検索したい年月(YYYYMM)
+	 * @param day 検索したい日付(1日 -> 1, 2日 -> 2...)
+	 * @return List<ScheduleUserDto><br>
+	 * フィールド(&lt;ScheduleUserDto&gt;)<br>
+	 * id, user, schedule1, schedule2, schedule3, schedule4, schedule5, schedule6, schedule7
+	 *
+	 */
+	private List<ScheduleUserDto> selectScheduleDay(String ym, String day) {
+
+		//検索したい文字と文字列目を指定するためにフォーマットを"%1______%"に整える
+		String schedule1 = Const.CHARACTER_PERCENT + Const.SCHEDULE_PRE_DAY_RECORDED + "______" + Const.CHARACTER_PERCENT;
+		String schedule2 = Const.CHARACTER_PERCENT + "_" + Const.SCHEDULE_PRE_DAY_RECORDED + "_____" + Const.CHARACTER_PERCENT;
+		String schedule3 = Const.CHARACTER_PERCENT + "__" + Const.SCHEDULE_PRE_DAY_RECORDED + "____" + Const.CHARACTER_PERCENT;
+		String schedule4 = Const.CHARACTER_PERCENT + "___" + Const.SCHEDULE_PRE_DAY_RECORDED + "___" + Const.CHARACTER_PERCENT;
+		String schedule5 = Const.CHARACTER_PERCENT + "____" + Const.SCHEDULE_PRE_DAY_RECORDED + "__" + Const.CHARACTER_PERCENT;
+		String schedule6 = Const.CHARACTER_PERCENT + "_____" + Const.SCHEDULE_PRE_DAY_RECORDED + "_" + Const.CHARACTER_PERCENT;
+		String schedule7 = Const.CHARACTER_PERCENT + "______" + Const.SCHEDULE_PRE_DAY_RECORDED + Const.CHARACTER_PERCENT;
+
+		List<ScheduleUserDto> scheduleDayDtoList = new ArrayList<>();
+
+		scheduleDayDtoList = scheduleUserRepository.selectScheduleDay1BySchedulRepalceValueYm(schedule1, schedule2, schedule3, schedule4, schedule5, schedule6, schedule7, Const.SCHEDULE_PRE_DAY_RECORDED, ym);
+		return scheduleDayDtoList;
 	}
 }
