7894e948290169268dab9e22a876258e245f6462
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-23 20:55:22 +0900

    fix: Entity内のメソッド追加

--- src/main/java/com/shift/domain/model/dto/ScheduleUserDto.java
+++ src/main/java/com/shift/domain/model/dto/ScheduleUserDto.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.shift.common.Const;
+
 import lombok.Data;
 
 /**
@@ -43,4 +45,33 @@ public class ScheduleUserDto {
 
 	@Column(name = "schedule7", unique = true)
 	private String schedule7;
+
+
+	//メソッド
+	public Boolean[] scheduleFormatIsRecordedArray() {
+
+		//scheduleをArrayに格納して返す
+		Boolean[] isScheduleArray = new Boolean[Const.SCHEDULE_RECORDABLE_MAX_DIVISION];
+		isScheduleArray[0] = isRecordedSchedule(schedule1);
+		isScheduleArray[1] = isRecordedSchedule(schedule2);
+		isScheduleArray[2] = isRecordedSchedule(schedule3);
+		isScheduleArray[3] = isRecordedSchedule(schedule4);
+		isScheduleArray[4] = isRecordedSchedule(schedule5);
+		isScheduleArray[5] = isRecordedSchedule(schedule6);
+		isScheduleArray[6] = isRecordedSchedule(schedule7);
+		return isScheduleArray;
+	}
+
+
+	//private 共通メソッド
+	private boolean isRecordedSchedule(String value) {
+
+		//登録されている(valueが1)のとき
+		if (Const.SCHEDULE_PRE_DAY_RECORDED.equals(value)) {
+			return true;
+		}
+
+		//登録されていないときはfalseを返す
+		return false;
+	}
 }
