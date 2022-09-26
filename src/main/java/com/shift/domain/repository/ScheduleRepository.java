e89b45a4778da2036ac23cb4ce21b6c7cf00f18a
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-26 12:27:41 +0900

    fix: SQL:確定スケジュール検索処理の追加

--- src/main/java/com/shift/domain/repository/ScheduleRepository.java
+++ src/main/java/com/shift/domain/repository/ScheduleRepository.java
@@ -18,7 +18,7 @@ public interface ScheduleRepository extends BaseRepository<ScheduleEntity, Integ
 	 *
 	 * <p>現在の年月からユーザーの1ヵ月分のスケジュール予定を取得する<br>
 	 * ただし、登録済みのスケジュールがないときはnullとなる<br>
-	 * なた、日付が存在しない日(2月 -> 30, 31日etc)は必ず登録されていない
+	 * また、日付が存在しない日(2月 -> 30, 31日etc)は必ず登録されていない
 	 * </p>
 	 *
 	 * @param ym 現在の年月(YYYYMM)
