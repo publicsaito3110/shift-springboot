def994756265df07000ca2f8e709fbc9684ee8d6
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-27 19:56:14 +0900

    add: LoginForgotIdSendBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/LoginForgotIdSendBean.java
@@ -0,0 +1,17 @@
+package com.shift.domain.model.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginForgotIdSendBean {
+
+	private boolean isSuccessSendEmail;
+}
