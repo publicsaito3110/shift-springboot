32e5c3fd9893abce9327ae9e75a992a22f6d9772
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-30 21:56:39 +0900

    fix: DmMenueRepositoryのSQL変更に伴うDtoの修正

--- src/main/java/com/shift/domain/model/dto/DmMenuDto.java
+++ src/main/java/com/shift/domain/model/dto/DmMenuDto.java
@@ -4,6 +4,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.shift.common.CommonUtil;
+import com.shift.common.Const;
+
 import lombok.Data;
 
 /**
@@ -28,4 +31,30 @@ public class DmMenuDto {
 
 	@Column(name = "msg_to_id", unique = true)
 	private String msgToId;
+
+	@Column(name = "icon_flg", unique = true)
+	private String iconFlg;
+
+
+	//メソッド
+	public String iconFlgFormatHtmlSrc() {
+
+		if (CommonUtil.isSuccessValidation(iconFlg, Const.PATTERN_USER_ICON_FLG_JPG)) {
+
+			//アイコンが登録済みかつ拡張子がjpgのときsrc(.jpg)を返す
+			return Const.HTML_SRC_USER_ICON_FOLDER_PATH + id + ".jpg";
+		} else if (CommonUtil.isSuccessValidation(iconFlg, Const.PATTERN_USER_ICON_FLG_JPEG)) {
+
+			//アイコンが登録済みかつ拡張子がjpegのときsrc(.jpeg)を返す
+			return Const.HTML_SRC_USER_ICON_FOLDER_PATH + id + ".jpeg";
+		} else if (CommonUtil.isSuccessValidation(iconFlg, Const.PATTERN_USER_ICON_FLG_PNG)) {
+
+			//アイコンが登録済みかつ拡張子がpngのときsrc(.png)を返す
+			return Const.HTML_SRC_USER_ICON_FOLDER_PATH + id + ".png";
+		} else {
+
+			//アイコンが登録済みでないときデフォルトアイコンのパスを返す
+			return Const.HTML_SRC_USER_DEFOULT_ICON;
+		}
+	}
 }
