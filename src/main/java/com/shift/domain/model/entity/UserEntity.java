733a2458fd974c17c7a90de43d29d020a1682161
publicsaito3110 <public.business.saito3110@gmail.com>
2022-09-30 13:07:21 +0900

    fix: テーブルuser: icon_flgの追加に伴う修正

--- src/main/java/com/shift/domain/model/entity/UserEntity.java
+++ src/main/java/com/shift/domain/model/entity/UserEntity.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.shift.common.CommonLogic;
 import com.shift.common.CommonUtil;
 import com.shift.common.Const;
 
@@ -50,6 +51,9 @@ public class UserEntity extends BaseEntity {
 	@Column(name = "note")
 	private String note;
 
+	@Column(name = "icon_flg")
+	private String iconFlg;
+
 	@Column(name = "admin_flg")
 	private String adminFlg;
 
@@ -62,12 +66,41 @@ public class UserEntity extends BaseEntity {
 
 		if (CommonUtil.isSuccessValidation(adminFlg, Const.PATTERN_ROLE_USER_ADMIN)) {
 
-			//管理者であるとき
+			//管理者であるとき、管理者であることを返す
 			return Const.ROLE_USER_ADMIN;
 		} else {
 
-			//一般ユーザーであるとき
+			//一般ユーザーであるとき、一般ユーザーであることを返す
 			return Const.ROLE_USER_GENERAL;
 		}
+	}
+
+
+	public String iconFlgFormatHtmlSrc() {
+
+		if (CommonUtil.isSuccessValidation(iconFlg, Const.PATTERN_USER_ICON_FLG_JPG)) {
+
+			//アイコンが登録済みかつ拡張子がjpgのときsrc(.jpg)を返す
+			return Const.HTML_SRC_USER_ICON_FOLDER_PATH + id + ".jpg";
+		} else if (CommonUtil.isSuccessValidation(iconFlg, Const.PATTERN_USER_ICON_FLG_JPEG)) {
+
+			//アイコンが登録済みかつ拡張子がjpegのときsrc(.jpeg)を返す
+			return Const.HTML_SRC_USER_ICON_FOLDER_PATH + id + ".jpeg";
+		} else if (CommonUtil.isSuccessValidation(iconFlg, Const.PATTERN_USER_ICON_FLG_PNG)) {
+
+			//アイコンが登録済みかつ拡張子がpngのときsrc(.png)を返す
+			return Const.HTML_SRC_USER_ICON_FOLDER_PATH + id + ".png";
+		} else {
+
+			//アイコンが登録済みでないときデフォルトアイコンのパスを返す
+			return Const.HTML_SRC_USER_DEFOULT_ICON;
+		}
+	}
+
+
+	public String noteFormatAfterBreakLine() {
+
+		//改行対応
+		return new CommonLogic().changeAfterBreakLine(note);
 	}
 }
