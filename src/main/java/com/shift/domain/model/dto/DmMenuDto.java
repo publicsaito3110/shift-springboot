84186a46153a8819e297708d231a361fd96a3109
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-02 19:55:54 +0900

    fix: ddl変更に伴うDtoの修正

--- src/main/java/com/shift/domain/model/dto/DmMenuDto.java
+++ src/main/java/com/shift/domain/model/dto/DmMenuDto.java
@@ -32,22 +32,22 @@ public class DmMenuDto {
 	@Column(name = "msg_to_id", unique = true)
 	private String msgToId;
 
-	@Column(name = "icon_flg", unique = true)
-	private String iconFlg;
+	@Column(name = "icon_kbn", unique = true)
+	private String iconKbn;
 
 
 	//メソッド
-	public String iconFlgFormatHtmlSrc() {
+	public String iconKbnFormatHtmlSrc() {
 
-		if (CommonUtil.isSuccessValidation(iconFlg, Const.PATTERN_USER_ICON_FLG_JPG)) {
+		if (CommonUtil.isSuccessValidation(iconKbn, Const.PATTERN_USER_ICON_KBN_JPG)) {
 
 			//アイコンが登録済みかつ拡張子がjpgのときsrc(.jpg)を返す
 			return Const.HTML_SRC_USER_ICON_FOLDER_PATH + id + ".jpg";
-		} else if (CommonUtil.isSuccessValidation(iconFlg, Const.PATTERN_USER_ICON_FLG_JPEG)) {
+		} else if (CommonUtil.isSuccessValidation(iconKbn, Const.PATTERN_USER_ICON_KBN_JPEG)) {
 
 			//アイコンが登録済みかつ拡張子がjpegのときsrc(.jpeg)を返す
 			return Const.HTML_SRC_USER_ICON_FOLDER_PATH + id + ".jpeg";
-		} else if (CommonUtil.isSuccessValidation(iconFlg, Const.PATTERN_USER_ICON_FLG_PNG)) {
+		} else if (CommonUtil.isSuccessValidation(iconKbn, Const.PATTERN_USER_ICON_KBN_PNG)) {
 
 			//アイコンが登録済みかつ拡張子がpngのときsrc(.png)を返す
 			return Const.HTML_SRC_USER_ICON_FOLDER_PATH + id + ".png";
