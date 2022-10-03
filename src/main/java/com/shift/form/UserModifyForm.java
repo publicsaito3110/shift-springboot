50b11c367912bffc28321be892acd84e2931515a
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-03 21:23:19 +0900

    fix: ファイルアップロード機能追加に伴うFormの修正

--- src/main/java/com/shift/form/UserModifyForm.java
+++ src/main/java/com/shift/form/UserModifyForm.java
@@ -3,20 +3,21 @@ package com.shift.form;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.shift.common.Const;
+import com.shift.domain.model.entity.UserEntity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author saito
  *
  */
 @Data
+@NoArgsConstructor
 public class UserModifyForm {
-
-	@NotBlank(message = "必須入力です")
-	@Pattern(regexp = Const.PATTERN_USER_ID_INPUT, message = "入力値が不正です")
-	private String userId;
 
 	@NotBlank(message = "必須入力です")
 	@Pattern(regexp = Const.PATTERN_USER_NAME_INPUT, message = "20文字以内で入力してください")
@@ -32,4 +33,15 @@ public class UserModifyForm {
 
 	@Pattern(regexp = Const.PATTERN_USER_NOTE_INPUT, message = "400文字以内で入力してください")
 	private String note;
+
+	private MultipartFile uploadFile;
+
+
+	//コンストラクタ
+	public UserModifyForm(UserEntity userEntity) {
+		name = userEntity.getName();
+		nameKana = userEntity.getNameKana();
+		gender = userEntity.getGender();
+		note = userEntity.getNote();
+	}
 }
