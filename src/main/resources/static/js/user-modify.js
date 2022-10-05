20104db018f6b0b861c3a73aa748ee563e57ab17
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-05 18:18:05 +0900

    fix: ファイルの許容可能な容量を判定する処理の追加

--- src/main/resources/static/js/user-modify.js
+++ src/main/resources/static/js/user-modify.js
@@ -1,9 +1,23 @@
 $(function(){
 
+	//ファイルアップロード可能な最大ファイルサイズ
+	const FILE_MAX_LIMIT_SIZE_BYTE = 1000000;
+
 	//-----------------------------
 	//ファイルアップロード時の処理
 	//-----------------------------
 	$("#icon-input").on("change", function (e) {
+
+		//アップロードされたファイルサイズを取得
+		var fileSize = this.files[0].size;
+
+		//ファイルサイズがアップロード可能最大ファイルサイズを超えたとき
+		if (FILE_MAX_LIMIT_SIZE_BYTE < fileSize) {
+
+			//ファイルサイズをBからMBへ換算し、アラートを表示
+			var fileSizeMB = fileSize / FILE_MAX_LIMIT_SIZE_BYTE;
+			alert("ファイルの容量が大きすぎます。1MB以下のファイルのみ有効です。\n" + fileSizeMB + "MB");
+		}
 
 		//画像を更新する変数
 		var fileReader = new FileReader();
