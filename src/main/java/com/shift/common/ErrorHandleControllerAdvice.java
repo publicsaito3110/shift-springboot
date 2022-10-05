47f1627c9ec11bdefbda9c5c1425245dec51427a
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-05 19:59:04 +0900

    fix: HttpStatusエラーを取得する処理を追加

--- src/main/java/com/shift/common/ErrorHandleControllerAdvice.java
+++ src/main/java/com/shift/common/ErrorHandleControllerAdvice.java
@@ -24,7 +24,7 @@ public class ErrorHandleControllerAdvice {
 	/**
 	 * エラーハンドリング処理
 	 *
-	 * <p>例外発生情報(Exception)を取得し、エラーログをlogファイルに出力し、エラー画面へ遷移させる<br>
+	 * <p>例外発生情報(Exception)を取得してエラーログをlogファイルに出力後、エラー画面へ遷移させる<br>
 	 * また、Exceptionのスーパークラスも含む<br>
 	 * ただし、HttpStatusは500になる
 	 * </p>
@@ -42,10 +42,11 @@ public class ErrorHandleControllerAdvice {
 		StackTraceElement[] stackTraceArray = e.getStackTrace();
 		String errorClassName = stackTraceArray[0].toString();
 
-		//HttpStatusを500に設定し、Statusコードを取得
+		//HttpStatusを500に設定し、Statusコード,Statusメッセージを取得
 		HttpServletResponse response = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getResponse();
 		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 		String httpStatus = String.valueOf(response.getStatus());
+		String httpStatusMessage = HttpStatus.INTERNAL_SERVER_ERROR.name();
 
 		//エラーログをlogファイルに出力
 		log.info("↓↓エラーログ↓↓");
@@ -57,6 +58,7 @@ public class ErrorHandleControllerAdvice {
 		modelAndView.addObject("exceptionClassName", exceptionClassName);
 		modelAndView.addObject("errorClassName", errorClassName);
 		modelAndView.addObject("httpStatus", httpStatus);
+		modelAndView.addObject("httpStatusMessage", httpStatusMessage);
 		modelAndView.addObject("errorMassage", "予期せぬエラーが発生しました");
 
 		modelAndView.setViewName("error");
@@ -67,7 +69,7 @@ public class ErrorHandleControllerAdvice {
 	/**
 	 * アップロードファイルサイズオーバーエラーハンドリング処理
 	 *
-	 * <p>例外発生情報(MaxUploadSizeExceededException)を取得し、エラーログをlogファイルに出力し、エラー画面へ遷移させる<br>
+	 * <p>例外発生情報(MaxUploadSizeExceededException)を取得し、エラー画面へ遷移させる<br>
 	 * また、MaxUploadSizeExceededExceptionが優先される<br>
 	 * ただし、HttpStatusは416になる
 	 * </p>
@@ -85,16 +87,18 @@ public class ErrorHandleControllerAdvice {
 		StackTraceElement[] stackTraceArray = e.getStackTrace();
 		String errorClassName = stackTraceArray[0].toString();
 
-		//HttpStatusを416に設定し、Statusコードを取得
+		//HttpStatusを416に設定し、Statusコード,Statusメッセージを取得
 		HttpServletResponse response = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getResponse();
 		response.setStatus(HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE.value());
 		String httpStatus = String.valueOf(response.getStatus());
+		String httpStatusMessage = HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE.name();
 
 		//ModelAndViewにエラー情報をセットし、error.htmlへ遷移
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.addObject("exceptionClassName", exceptionClassName);
 		modelAndView.addObject("errorClassName", errorClassName);
 		modelAndView.addObject("httpStatus", httpStatus);
+		modelAndView.addObject("httpStatusMessage", httpStatusMessage);
 		modelAndView.addObject("errorMassage", "ファイルの容量が大きすぎます。1MB以下のファイルのみ有効です。");
 
 		modelAndView.setViewName("error");
