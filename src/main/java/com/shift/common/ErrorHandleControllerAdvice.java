41dd7b22774c4e1cb74ea26c947edcbbc515a551
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-05 18:22:53 +0900

    fix: MaxUploadSizeExceededExceptionのエラー対応

--- src/main/java/com/shift/common/ErrorHandleControllerAdvice.java
+++ src/main/java/com/shift/common/ErrorHandleControllerAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
@@ -57,6 +58,44 @@ public class ErrorHandleControllerAdvice {
 		modelAndView.addObject("errorClassName", errorClassName);
 		modelAndView.addObject("httpStatus", httpStatus);
 		modelAndView.addObject("errorMassage", "予期せぬエラーが発生しました");
+
+		modelAndView.setViewName("error");
+		return modelAndView;
+	}
+
+
+	/**
+	 * アップロードファイルサイズオーバーエラーハンドリング処理
+	 *
+	 * <p>例外発生情報(MaxUploadSizeExceededException)を取得し、エラーログをlogファイルに出力し、エラー画面へ遷移させる<br>
+	 * また、MaxUploadSizeExceededExceptionが優先される<br>
+	 * ただし、HttpStatusは416になる
+	 * </p>
+	 *
+	 * @param e Exception
+	 * @return ModelAndView
+	 */
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ModelAndView maxUploadSizeExceededExceptionHandler(Exception e) {
+
+		//Exceptionクラスを取得
+		String exceptionClassName = e.toString();
+
+		//Exception発生個所を取得
+		StackTraceElement[] stackTraceArray = e.getStackTrace();
+		String errorClassName = stackTraceArray[0].toString();
+
+		//HttpStatusを416に設定し、Statusコードを取得
+		HttpServletResponse response = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getResponse();
+		response.setStatus(HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE.value());
+		String httpStatus = String.valueOf(response.getStatus());
+
+		//ModelAndViewにエラー情報をセットし、error.htmlへ遷移
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("exceptionClassName", exceptionClassName);
+		modelAndView.addObject("errorClassName", errorClassName);
+		modelAndView.addObject("httpStatus", httpStatus);
+		modelAndView.addObject("errorMassage", "ファイルの容量が大きすぎます。1MB以下のファイルのみ有効です。");
 
 		modelAndView.setViewName("error");
 		return modelAndView;
