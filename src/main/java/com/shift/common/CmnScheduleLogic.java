4ba8dc3be229e74e36b399a32dfbb8edce3c76e0
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-06 20:07:46 +0900

    fix: schedule登録済みユーザを取得するメソッドの追加

--- src/main/java/com/shift/common/CmnScheduleLogic.java
+++ src/main/java/com/shift/common/CmnScheduleLogic.java
@@ -2,6 +2,7 @@ package com.shift.common;
 
 import java.util.List;
 
+import com.shift.domain.model.entity.ScheduleEntity;
 import com.shift.domain.model.entity.ScheduleTimeEntity;
 
 /**
@@ -69,5 +70,66 @@ public class CmnScheduleLogic {
 		}
 
 		return isScheduleRecordedArray;
+	}
+
+
+	/**
+	 * 確定スケジュール登録済みユーザArray処理
+	 *
+	 * <p>scheduleEntityListとscheduleTimeListから登録済みのスケジュールとスケジュール時間区分を取得し、登録されているユーザをArrayで取得する<br>
+	 * ただし、scheduleTimeListの要素数だけ判別する(最大7まで)<br>
+	 * 日付とスケジュール時間区分に登録しているユーザが存在するとき、ユーザー名を格納する(ユーザ毎に改行タグ&lt;br&gt;が追加される)
+	 * </p>
+	 *
+	 * @param scheduleEntityList DBから取得したList<ScheduleEntity> (List&lt;ScheduleEntity&gt;)
+	 * @param scheduleTimeList DBから取得したList<ScheduleTimeEntity> (List&lt;ScheduleTimeEntity&gt;)
+	 * @return String[][]<br>
+	 * エレメント(String[日付(31固定)][スケジュール時間(スケジュール登録可能数)])<br>
+	 * 日付とスケジュール時間区分に登録しているユーザ名が順次格納される
+	 */
+	public String[][] calcUserALLScheduleArrayBySchedule(List<ScheduleEntity> scheduleEntityList, List<ScheduleTimeEntity> scheduleTimeList) {
+
+		//スケジュールに登録されているユーザを格納するための変数(要素[日付][スケジュール時間区分])
+		String[][] userScheduleAllArray = new String[31][Const.SCHEDULE_RECORDABLE_MAX_DIVISION];
+
+		//スケジュールの判定を行うための共通クラス
+		CmnScheduleLogic cmnScheduleLogic = new CmnScheduleLogic();
+
+		//scheduleEntityListの要素数(ユーザ数)だけループする
+		for (ScheduleEntity scheduleEntity: scheduleEntityList) {
+
+			//登録されているユーザ名を取得
+			String userName = scheduleEntity.getUser();
+
+			//ユーザの1ヵ月分のスケジュールを日付ごとに取得
+			List<String> scheduleDayList = scheduleEntity.getDayList();
+
+			//scheduleDayList(日付の回数)だけループする
+			for (int i = 0; i < scheduleDayList.size(); i++) {
+
+				//日付ごとのスケジュールを取得し、スケジュール時間ごとにスケジュールが登録されているか判定する
+				String scheduleDay = scheduleDayList.get(i);
+				Boolean[] isScheduleRecordedArray = cmnScheduleLogic.toIsScheduleRecordedArrayBySchedule(scheduleDay, scheduleTimeList);
+
+				//isScheduleRecordedArray(スケジュール時間の区分)だけループする
+				for (int j = 0; j < isScheduleRecordedArray.length; j++) {
+
+					//isScheduleRecordedArray[j]がnull(スケジュール時間区分がない)とき、何もせずisScheduleRecordedArrayのループに戻る
+					if (isScheduleRecordedArray[j] == null) {
+						continue;
+					}
+
+					//スケジュールが登録されているとき
+					if (isScheduleRecordedArray[j]) {
+
+						//スケジュールに登録されているユーザに現在のユーザを改行タグ(<br>)と共に追加する
+						String userSchedule = CommonUtil.changeEmptyByNull(userScheduleAllArray[i][j]) +  userName + Const.HTML_TAG_BR;
+						userScheduleAllArray[i][j] = userSchedule;
+					}
+				}
+			}
+		}
+
+		return userScheduleAllArray;
 	}
 }
