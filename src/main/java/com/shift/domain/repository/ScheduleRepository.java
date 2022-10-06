fb8f10455f8fc570b0d0ca38d3fd2704578275f8
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-06 20:12:51 +0900

    fix: SQL: スケジュール登録済みユーザ取得処理の追加

--- src/main/java/com/shift/domain/repository/ScheduleRepository.java
+++ src/main/java/com/shift/domain/repository/ScheduleRepository.java
@@ -1,6 +1,8 @@
 package com.shift.domain.repository;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.shift.domain.model.entity.ScheduleEntity;
@@ -14,7 +16,7 @@ public interface ScheduleRepository extends BaseRepository<ScheduleEntity, Integ
 
 
 	/**
-	 * [DB]スケジュール検索処理
+	 * [DB]ユーザの確定スケジュール検索処理
 	 *
 	 * <p>現在の年月からユーザーの1ヵ月分のスケジュール予定を取得する<br>
 	 * ただし、登録済みのスケジュールがないときはnullとなる<br>
@@ -28,4 +30,21 @@ public interface ScheduleRepository extends BaseRepository<ScheduleEntity, Integ
 	 * id, ym, user, 1, 2, 3, 4, 5... 30, 31
 	 */
 	public ScheduleEntity findByYmAndUser(String ym, String loginUser);
+
+
+	/**
+	 * [DB]全ユーザのスケジュール検索処理
+	 *
+	 * <p>現在の年月から全てのユーザーの1ヵ月分のスケジュール予定を取得する<br>
+	 * ただし、登録済みのスケジュールがないときはnullとなる<br>
+	 * また、日付が存在しない日(2月 -> 30, 31日etc)は必ず登録されていない
+	 * </p>
+	 *
+	 * @param ym 現在の年月(YYYYMM)
+	 * @param loginUser ログインしているユーザー
+	 * @return List<ScheduleEntity> <br>
+	 * フィールド(List&lt;ScheduleEntity&gt;)<br>
+	 * id, ym, user, 1, 2, 3, 4, 5... 30, 31
+	 */
+	public List<ScheduleEntity> findByYm(String ym);
 }
