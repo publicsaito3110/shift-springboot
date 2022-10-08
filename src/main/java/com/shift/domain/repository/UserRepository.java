35dfc136869438d97402dd8f3db32df39d721e34
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-08 19:50:46 +0900

    fix: SQL: id OR emailが一致するユーザを取得処理追加

--- src/main/java/com/shift/domain/repository/UserRepository.java
+++ src/main/java/com/shift/domain/repository/UserRepository.java
@@ -29,25 +29,10 @@ public interface UserRepository extends BaseRepository<UserEntity, String> {
 	 * ただし、LIKE句であるため、"%keyword%" でなければならない
 	 * @return List<UserEntity><br>
 	 * フィールド(List&lt;UserEntity&gt;)<br>
-	 * id, name, nameKana, gender, password, address, tel, email, note, admin_flg, del_flg
+	 * id, name, nameKana, gender, password, address, tel, email, note, icon_kbn, admin_flg, del_flg
 	 */
 	@Query(value = "SELECT u.* FROM user u WHERE u.id != :loginUser AND (u.del_flg != :delFlg OR u.del_flg IS NULL) AND (u.id LIKE :keyword OR u.name LIKE :keyword OR u.name_kana LIKE :keyword) ORDER BY u.id", nativeQuery = true)
 	public List<UserEntity> selectUserByKeywordNotUserIdDelFlg(String loginUser, String delFlg, String keyword);
-
-
-	/**
-	 * [DB]ユーザ検索処理
-	 *
-	 * <p>emailと一致するユーザを取得する<br>
-	 * ただし、一致するユーザーがいない場合はnullとなる
-	 * </p>
-	 *
-	 * @param email RequestParameter
-	 * @return UserEntity<br>
-	 * フィールド(UserEntity)<br>
-	 * id, name, nameKana, gender, password, address, tel, email, note, admin_flg, del_flg
-	 */
-	public UserEntity findByEmail(String email);
 
 
 	/**
@@ -61,7 +46,38 @@ public interface UserRepository extends BaseRepository<UserEntity, String> {
 	 * @param delFlg 退職フラグの値
 	 * @return List<UserEntity><br>
 	 * フィールド(List&lt;UserEntity&gt;)<br>
-	 * id, name, nameKana, gender, password, address, tel, email, note, admin_flg, del_flg
+	 * id, name, nameKana, gender, password, address, tel, email, note, icon_kbn, admin_flg, del_flg
 	 */
 	public List<UserEntity> findByDelFlgNotOrDelFlgIsNull(String delFlg);
+
+
+	/**
+	 * [DB]ユーザ検索処理
+	 *
+	 * <p>emailと一致するユーザを取得する<br>
+	 * ただし、一致するユーザーがいない場合はnullとなる
+	 * </p>
+	 *
+	 * @param email 検索したいメールアドレス
+	 * @return UserEntity<br>
+	 * フィールド(UserEntity)<br>
+	 * id, name, nameKana, gender, password, address, tel, email, note, icon_kbn, admin_flg, del_flg
+	 */
+	public UserEntity findByEmail(String email);
+
+
+	/**
+	 * [DB]ユーザ検索処理
+	 *
+	 * <p>idまたはemailのどちらかと一致するユーザを取得する<br>
+	 * ただし、一致するユーザーがいない場合はEmptyとなる
+	 * </p>
+	 *
+	 * @param id 検索したいID
+	 * @param email 検索したいメールアドレス
+	 * @return List<UserEntity> <br>
+	 * フィールド(List&lt;UserEntity&gt;)<br>
+	 * id, name, nameKana, gender, password, address, tel, email, note, icon_kbn, admin_flg, del_flg
+	 */
+	public List<UserEntity> findByIdOrEmail(String id, String email);
 }
