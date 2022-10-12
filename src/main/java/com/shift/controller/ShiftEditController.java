a6030d723bd4ec27585d442507f8813c95775947
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-12 21:52:53 +0900

    fix: RequestMappingの追加に伴うControllerの修正

--- src/main/java/com/shift/controller/ShiftEditController.java
+++ src/main/java/com/shift/controller/ShiftEditController.java
@@ -3,12 +3,17 @@ package com.shift.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.shift.common.Const;
 import com.shift.domain.model.bean.ShiftEditBean;
 import com.shift.domain.service.ShiftEditService;
+import com.shift.form.ShiftEditModifyForm;
 
 /**
  * @author saito
@@ -35,9 +40,44 @@ public class ShiftEditController extends BaseController {
 		//Service
 		ShiftEditBean shiftEditBean = shiftEdiService.shiftEdit();
 		modelAndView.addObject("scheduleTimeEntity", shiftEditBean.getScheduleTimeEntity());
-		modelAndView.addObject("scheduleTimeDivision", Const.SCHEDULE_RECORDABLE_MAX_DIVISION);
+		modelAndView.addObject("shiftEditModifyForm", new ShiftEditModifyForm());
 		modelAndView.addObject("scheduleTimeHtmlClassBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY);
+		//View
+		modelAndView.setViewName("shift-edit");
+		return modelAndView;
+	}
 
+
+	/**
+	 * スケジュール時間修正機能<br>
+	 * [Controller] (/shift-edit/modify)
+	 *
+	 * @param authentication Authentication
+	 * @param modelAndView ModelAndView
+	 * @return ModelAndView
+	 */
+	@RequestMapping(value = "/shift-edit/modify", method = RequestMethod.POST)
+	public ModelAndView shiftEditModify(@Validated @ModelAttribute ShiftEditModifyForm shiftEditModifyForm, BindingResult bindingResult, Authentication authentication, ModelAndView modelAndView) {
+
+		//バリデーションエラーのとき
+		if (bindingResult.hasErrors()) {
+
+			//Service
+			ShiftEditBean shiftEditBean = shiftEdiService.shiftEdit();
+			modelAndView.addObject("scheduleTimeEntity", shiftEditBean.getScheduleTimeEntity());
+			modelAndView.addObject("shiftEditModifyForm", new ShiftEditModifyForm());
+			modelAndView.addObject("scheduleTimeHtmlClassBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY);
+			//View
+			modelAndView.setViewName("shift-edit");
+			return modelAndView;
+		}
+
+		//Service
+		ShiftEditBean shiftEditBean = shiftEdiService.shiftEdit();
+		modelAndView.addObject("scheduleTimeEntity", shiftEditBean.getScheduleTimeEntity());
+		modelAndView.addObject("shiftEditModifyForm", new ShiftEditModifyForm());
+		modelAndView.addObject("scheduleTimeHtmlClassBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY);
+		//View
 		modelAndView.setViewName("shift-edit");
 		return modelAndView;
 	}
