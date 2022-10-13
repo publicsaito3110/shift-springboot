b0939205cef3027ee67c9f1c1d603cf64086d1ee
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-13 21:12:49 +0900

    fix: ShiftEditServiceの変更に伴うControllerの修正

--- src/main/java/com/shift/controller/ShiftEditController.java
+++ src/main/java/com/shift/controller/ShiftEditController.java
@@ -11,9 +11,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.shift.common.Const;
+import com.shift.domain.model.bean.ShiftEditAddBean;
 import com.shift.domain.model.bean.ShiftEditBean;
 import com.shift.domain.service.ShiftEditService;
-import com.shift.form.ShiftEditModifyForm;
+import com.shift.form.ShiftEditAddForm;
 
 /**
  * @author saito
@@ -40,8 +41,9 @@ public class ShiftEditController extends BaseController {
 		//Service
 		ShiftEditBean shiftEditBean = shiftEdiService.shiftEdit();
 		modelAndView.addObject("scheduleTimeEntity", shiftEditBean.getScheduleTimeEntity());
-		modelAndView.addObject("shiftEditModifyForm", new ShiftEditModifyForm());
+		modelAndView.addObject("shiftEditAddForm", new ShiftEditAddForm());
 		modelAndView.addObject("scheduleTimeHtmlClassBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY);
+		modelAndView.addObject("isModalResult", false);
 		//View
 		modelAndView.setViewName("shift-edit");
 		return modelAndView;
@@ -50,14 +52,15 @@ public class ShiftEditController extends BaseController {
 
 	/**
 	 * スケジュール時間修正機能<br>
-	 * [Controller] (/shift-edit/modify)
+	 * [Controller] (/shift-edit/add)
 	 *
+	 * @param shiftEditAddForm RequestParam Form
 	 * @param authentication Authentication
 	 * @param modelAndView ModelAndView
 	 * @return ModelAndView
 	 */
-	@RequestMapping(value = "/shift-edit/modify", method = RequestMethod.POST)
-	public ModelAndView shiftEditModify(@Validated @ModelAttribute ShiftEditModifyForm shiftEditModifyForm, BindingResult bindingResult, Authentication authentication, ModelAndView modelAndView) {
+	@RequestMapping(value = "/shift-edit/add", method = RequestMethod.POST)
+	public ModelAndView shiftEditAdd(@Validated @ModelAttribute ShiftEditAddForm shiftEditAddForm, BindingResult bindingResult, Authentication authentication, ModelAndView modelAndView) {
 
 		//バリデーションエラーのとき
 		if (bindingResult.hasErrors()) {
@@ -65,18 +68,40 @@ public class ShiftEditController extends BaseController {
 			//Service
 			ShiftEditBean shiftEditBean = shiftEdiService.shiftEdit();
 			modelAndView.addObject("scheduleTimeEntity", shiftEditBean.getScheduleTimeEntity());
-			modelAndView.addObject("shiftEditModifyForm", new ShiftEditModifyForm());
+			modelAndView.addObject("shiftAddModifyForm", new ShiftEditAddForm());
 			modelAndView.addObject("scheduleTimeHtmlClassBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY);
+			modelAndView.addObject("isModalResult", true);
+			modelAndView.addObject("modalResultTitle", "勤務時間の新規追加結果");
+			modelAndView.addObject("modalResultContentFail", "入力値が不正です。勤務時間の新規追加に失敗しました。");
 			//View
 			modelAndView.setViewName("shift-edit");
 			return modelAndView;
 		}
 
 		//Service
-		ShiftEditBean shiftEditBean = shiftEdiService.shiftEdit();
-		modelAndView.addObject("scheduleTimeEntity", shiftEditBean.getScheduleTimeEntity());
-		modelAndView.addObject("shiftEditModifyForm", new ShiftEditModifyForm());
+		ShiftEditAddBean shiftEditAddBean = shiftEdiService.shiftEditAdd(shiftEditAddForm);
+
+		//新規のスケジュール時間区分の追加に失敗したとき
+		if (!shiftEditAddBean.isInsertScheduleTime()) {
+
+			modelAndView.addObject("scheduleTimeEntity", shiftEditAddBean.getScheduleTimeEntity());
+			modelAndView.addObject("shiftEditAddForm", new ShiftEditAddForm());
+			modelAndView.addObject("scheduleTimeHtmlClassBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY);
+			modelAndView.addObject("isModalResult", true);
+			modelAndView.addObject("modalResultTitle", "勤務時間の新規追加結果");
+			modelAndView.addObject("modalResultContentFail", "勤務時間の新規追加に失敗しました。");
+			//View
+			modelAndView.setViewName("shift-edit");
+			return modelAndView;
+		}
+
+		//新規のスケジュール時間区分の追加が成功したとき
+		modelAndView.addObject("scheduleTimeEntity", shiftEditAddBean.getScheduleTimeEntity());
+		modelAndView.addObject("shiftEditAddForm", new ShiftEditAddForm());
 		modelAndView.addObject("scheduleTimeHtmlClassBgColorArray", Const.SCHEDULE_HTML_CLASS_DISPLAY_BG_COLOR_ARRAY);
+		modelAndView.addObject("isModalResult", true);
+		modelAndView.addObject("modalResultTitle", "勤務時間の新規追加結果");
+		modelAndView.addObject("modalResultContentSuccess", "勤務時間の新規追加に成功しました。");
 		//View
 		modelAndView.setViewName("shift-edit");
 		return modelAndView;
