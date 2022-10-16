8c6d7ca2725203b68d7e1ccd956999b4dd077192
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-16 18:32:28 +0900

    fix: SQL:最終チャット履歴から既読メッセージ数を追加

--- src/main/java/com/shift/domain/repository/DmMenuRepository.java
+++ src/main/java/com/shift/domain/repository/DmMenuRepository.java
@@ -24,10 +24,11 @@ public interface DmMenuRepository extends BaseRepository<DmMenuDto, Integer>{
 	 * </p>
 	 *
 	 * @param loginUser 現在ログインしているユーザのID
+	 * @param readFlg 既読済み(red_flg)の値
 	 * @return List<DmChatDto><br>
 	 * フィールド(List&lt;DmMenuDto&gt;)<br>
-	 * id, msg, msg_to_name, msg_to_id, icon_kbn
+	 * id, msg, msg_to_name, msg_to_id, icon_kbn, unreadCount
 	 */
-	@Query(value = "SELECT a.*, s.icon_kbn FROM (SELECT DISTINCT d.id, d.msg, CASE WHEN d.send_user = :loginUser THEN s.name WHEN d.receive_user = :loginUser THEN u.name END AS msg_to_name, CASE WHEN d.send_user = :loginUser THEN s.id WHEN d.receive_user = :loginUser THEN u.id END AS msg_to_id FROM (dm d INNER JOIN USER u ON u.id = d.send_user) INNER JOIN USER s ON s.id = d.receive_user WHERE d.send_user = :loginUser OR d.receive_user = :loginUser ORDER BY d.id DESC) a INNER JOIN user s ON s.id = a.msg_to_id GROUP BY a.msg_to_id", nativeQuery = true)
-	public List<DmMenuDto> selectDmTalkHistoryByLoginUser(String loginUser);
+	@Query(value = "SELECT a.*, (SELECT COUNT(m.id) FROM dm m WHERE m.receive_user = :loginUser AND m.send_user = a.msg_to_id AND (m.read_flg != :readFlg OR m.read_flg IS NULL)) AS unread_count FROM (SELECT DISTINCT d.id, d.msg, CASE WHEN d.send_user = :loginUser THEN s.name WHEN d.receive_user = :loginUser THEN u.name END AS msg_to_name, CASE WHEN d.send_user = :loginUser THEN s.id WHEN d.receive_user = :loginUser THEN u.id END AS msg_to_id, CASE WHEN d.send_user = :loginUser THEN s.icon_kbn WHEN d.receive_user = :loginUser THEN u.icon_kbn END AS icon_kbn FROM (dm d INNER JOIN USER u ON u.id = d.send_user) INNER JOIN USER s ON s.id = d.receive_user WHERE d.send_user = :loginUser OR d.receive_user = :loginUser ORDER BY d.id DESC) a GROUP BY a.msg_to_id", nativeQuery = true)
+	public List<DmMenuDto> selectDmTalkHistoryByLoginUser(String loginUser, String readFlg);
 }
