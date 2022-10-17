eee4a92601ad22f2c895363f3a1ee9d297509b85
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-17 20:09:46 +0900

    fix: スケジュール機能を有効化するように修正

--- src/main/java/com/shift/ShiftBootApplication.java
+++ src/main/java/com/shift/ShiftBootApplication.java
@@ -2,11 +2,13 @@ package com.shift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author saito
  *
  */
+@EnableScheduling
 @SpringBootApplication
 public class ShiftBootApplication {
 
