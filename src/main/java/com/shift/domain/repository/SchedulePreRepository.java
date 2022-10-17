8ae9f243644edeb97195dd91613737b305851bae
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-17 20:12:36 +0900

    fix: SQL:登録済みユーザ予定スケジュール取得処理追加

--- src/main/java/com/shift/domain/repository/SchedulePreRepository.java
+++ src/main/java/com/shift/domain/repository/SchedulePreRepository.java
@@ -1,6 +1,8 @@
 package com.shift.domain.repository;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.shift.domain.model.entity.SchedulePreEntity;
@@ -12,12 +14,29 @@ import com.shift.domain.model.entity.SchedulePreEntity;
 @Repository
 public interface SchedulePreRepository extends BaseRepository<SchedulePreEntity, Integer> {
 
+
 	/**
-	 * [DB]スケジュール検索処理
+	 * [DB]スケジュール予定検索処理
+	 *
+	 * <p>現在の年月から提出済みの全ユーザのスケジュール予定を取得する<br>
+	 * ただし、登録済みのスケジュール予定がないときはEmptyとなる<br>
+	 * また、日付が存在しない日(2月 -> 30, 31日etc)は必ず登録されていない
+	 * </p>
+	 *
+	 * @param ym 現在の年月(YYYYMM)
+	 * @return List<SchedulePreEntity> <br>
+	 * フィールド(List&lt;SchedulePreEntity&gt;)<br>
+	 * id, ym, user, 1, 2, 3, 4, 5... 30, 31
+	 */
+	public List<SchedulePreEntity> findByYm(String ym);
+
+
+	/**
+	 * [DB]スケジュール予定検索処理
 	 *
 	 * <p>現在の年月からユーザーの1ヵ月分のスケジュール予定を取得する<br>
 	 * ただし、登録済みのスケジュールがないときはnullとなる<br>
-	 * なた、日付が存在しない日(2月 -> 30, 31日etc)は必ず登録されていない
+	 * また、日付が存在しない日(2月 -> 30, 31日etc)は必ず登録されていない
 	 * </p>
 	 *
 	 * @param ym 現在の年月(YYYYMM)
