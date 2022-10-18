87185ebb4e1e0e7027a0f3a17db357676f5149e3
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-18 20:28:56 +0900

    fix: 相関チェックの追加に伴う修正

--- src/main/java/com/shift/form/NewsEditModifyForm.java
+++ src/main/java/com/shift/form/NewsEditModifyForm.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import com.shift.common.Const;
+import com.shift.common.annotation.NotNgChar;
 
 import lombok.Data;
 
@@ -17,19 +18,17 @@ public class NewsEditModifyForm {
 	@NotBlank(message = "必須入力です")
 	private String id;
 
-	@NotBlank(message = "必須入力です")
 	@Pattern(regexp = Const.PATTERN_NEWS_YMD_INPUT, message = "入力値が不正です")
 	private String ymd;
 
-	@NotBlank(message = "必須入力です")
+	@NotNgChar(message = "入力禁止文字が含まれています")
 	@Pattern(regexp = Const.PATTERN_NEWS_TITLE_INPUT, message = "20文字以内のみ有効です")
 	private String title;
 
-	@NotBlank(message = "必須入力です")
 	@Pattern(regexp = Const.PATTERN_NEWS_CATEGORY_INPUT, message = "入力値が不正です")
 	private String category;
 
-	@NotBlank(message = "必須入力です")
+	@NotNgChar(message = "入力禁止文字が含まれています")
 	@Pattern(regexp = Const.PATTERN_NEWS_CONTENT_INPUT, message = "200文字以内のみ有効です")
 	private String content;
 }
