c947118bf6d9501eb747858ea66d2aa05a39e72d
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-20 19:24:47 +0900

    fix: 定数の追加

--- src/main/java/com/shift/common/Const.java
+++ src/main/java/com/shift/common/Const.java
@@ -25,6 +25,7 @@ public class Const {
 	public static final int HOME_NEWS_DISPLAY_NEW_ICON_LIMIT_DATE = 14;
 	public static final String HOME_NEWS_NEW_ICON_SRC = "/png/icon/new-icon.png";
 	public static final String HOME_NEWS_CATEGORY_ICON_SRC = "/png/icon/category-icon";
+	public static final int HOME_DISPLAY_SCHEDULE_DAY = 7;
 
 
 	//---------
