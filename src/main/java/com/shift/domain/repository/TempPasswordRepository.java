77cc682d05de2b6892599b16dce0ca453c35c6c6
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-23 19:30:29 +0900

    fix: SQL:ワンタイムパスワード情報取得処理の追加

--- src/main/java/com/shift/domain/repository/TempPasswordRepository.java
+++ src/main/java/com/shift/domain/repository/TempPasswordRepository.java
@@ -1,6 +1,8 @@
 package com.shift.domain.repository;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.shift.domain.model.entity.TempPasswordEntity;
@@ -16,32 +18,56 @@ public interface TempPasswordRepository extends BaseRepository<TempPasswordEntit
 	/**
 	 * [DB]ワンタイムパスワード情報検索処理
 	 *
+	 * <p>userに一致するワンタイムパスワード情報を取得する<br>
+	 * ただし、許容可能日時の範囲内ではないまたは一致するワンタイムパスワード情報がない場合はnullとなる<br>
+	 * また、minDate, maxDateがMySQLで許容されている日時フォーマット出ない場合はエラーが発生する
+	 * </p>
+	 *
+	 * @param user パスワードを再設定する際に登録したユーザID
+	 * @param minDate 検索最小日時
+	 * @param maxDate 検索最大日時
+	 * @return List<TempPasswordEntity><br>
+	 * フィールド(TempPasswordEntity)<br>
+	 * id, user, urlParam, authCode, insertDate
+	 */
+	public List<TempPasswordEntity> findByUserAndInsertDateBetween(String user, String minDate, String maxDate);
+
+
+	/**
+	 * [DB]ワンタイムパスワード情報検索処理
+	 *
 	 * <p>userとurlParamに一致するワンタイムパスワード情報を取得する<br>
-	 * ただし、一致するワンタイムパスワード情報がないときはnullになる
+	 * ただし、許容可能日時の範囲内ではないまたは一致するワンタイムパスワード情報がない場合はnullとなる<br>
+	 * また、minDate, maxDateがMySQLで許容されている日時フォーマット出ない場合はエラーが発生する
 	 * </p>
 	 *
 	 * @param user パスワードを再設定する際に登録したユーザID
 	 * @param urlParam パスワードを再設定する際に登録したURLパラメータ
+	 * @param minDate 検索最小日時
+	 * @param maxDate 検索最大日時
 	 * @return TempPasswordEntity<br>
 	 * フィールド(TempPasswordEntity)<br>
 	 * id, user, urlParam, authCode, insertDate
 	 */
-	public TempPasswordEntity findByUserAndUrlParam(String user, String urlParam);
+	public TempPasswordEntity findByUserAndUrlParamAndInsertDateBetween(String user, String urlParam, String minDate, String maxDate);
 
 
 	/**
 	 * [DB]ワンタイムパスワード情報検索処理
 	 *
 	 * <p>user, urlParam, authCodeに一致するワンタイムパスワード情報を取得する<br>
-	 * ただし、一致するワンタイムパスワード情報がないときはnullになる
+	 * ただし、許容可能日時の範囲内ではないまたは一致するワンタイムパスワード情報がない場合はnullとなる<br>
+	 * また、minDate, maxDateがMySQLで許容されている日時フォーマット出ない場合はエラーが発生する
 	 * </p>
 	 *
 	 * @param user パスワードを再設定する際に登録したユーザID
 	 * @param urlParam パスワードを再設定する際に登録したURLパラメータ
 	 * @param authCode パスワードを再設定する際に登録した認証コード
+	 * @param minDate 検索最小日時
+	 * @param maxDate 検索最大日時
 	 * @return TempPasswordEntity<br>
 	 * フィールド(TempPasswordEntity)<br>
 	 * id, user, urlParam, authCode, insertDate
 	 */
-	public TempPasswordEntity findByUserAndUrlParamAndAuthCode(String userId, String urlParam, String authCode);
+	public TempPasswordEntity findByUserAndUrlParamAndAuthCodeAndInsertDateBetween(String userId, String urlParam, String authCode, String minDate, String maxDate);
 }
