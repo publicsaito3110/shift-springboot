73cc625a482857b8ad793a22bb17f864596cac75
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-23 14:21:26 +0900

    add: LoginForgotPasswordResetBeanの追加

---
+++ src/main/java/com/shift/domain/model/bean/LoginForgotPasswordResetBean.java
@@ -0,0 +1,21 @@
+package com.shift.domain.model.bean;
+
+import com.shift.domain.model.entity.TempPasswordEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginForgotPasswordResetBean {
+
+	private boolean isTempPasswordAuth;
+
+	private TempPasswordEntity tempPasswordEntity;
+}
