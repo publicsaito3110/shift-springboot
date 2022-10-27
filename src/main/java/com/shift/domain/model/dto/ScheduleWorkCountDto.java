5ee14dfcbafadf45d0453f7dc67fbefbc866dfe0
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-27 21:26:28 +0900

    add: ScheduleWorkCountDtoの追加

---
+++ src/main/java/com/shift/domain/model/dto/ScheduleWorkCountDto.java
@@ -0,0 +1,28 @@
+package com.shift.domain.model.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+/**
+ * @author saito
+ *
+ */
+@Entity
+@Data
+public class ScheduleWorkCountDto {
+
+
+	//フィールド
+	@Id
+	@Column(name = "user_id", unique = true)
+	private String userId;
+
+	@Column(name = "user_name", unique = true)
+	private String userName;
+
+	@Column(name = "work_count", unique = true)
+	private int workCount;
+}
