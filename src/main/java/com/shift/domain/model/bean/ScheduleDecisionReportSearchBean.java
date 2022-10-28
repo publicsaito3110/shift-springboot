41b5334cf2eb0360af30acfe1d97a47d5d773e2a
publicsaito3110 <public.business.saito3110@gmail.com>
2022-10-28 19:25:40 +0900

    add: ScheduleDecisionReportSearchの追加

---
+++ src/main/java/com/shift/domain/model/bean/ScheduleDecisionReportSearchBean.java
@@ -0,0 +1,25 @@
+package com.shift.domain.model.bean;
+
+import java.util.List;
+
+import com.shift.domain.model.dto.ScheduleWorkCountMonthDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author saito
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleDecisionReportSearchBean {
+
+	private int year;
+
+	private int month;
+
+	private List<ScheduleWorkCountMonthDto> scheduleWorkCountMonthList;
+}
