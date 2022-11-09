3cab0d22efbf7e03578cdd847a6421dd53d1177c
publicsaito3110 <public.business.saito3110@gmail.com>
2022-11-09 21:30:26 +0900

    fix: 勤務時間の取得メソッドの追加に伴う修正

--- src/main/java/com/shift/domain/model/bean/ScheduleTimeBean.java
+++ src/main/java/com/shift/domain/model/bean/ScheduleTimeBean.java
@@ -23,23 +23,125 @@ public class ScheduleTimeBean {
 	private String restHm;
 
 
-	//メソッド
+	/**
+	 * startHm時間フォーマット変換処理<br>
+	 * 時間フォーマット(HH:MM)に変換
+	 *
+	 * @param void
+	 * @return String 時間フォーマット(HH:MM)
+	 */
 	public String startHmsFormatTime() {
-		return hmFormatTime(startHm);
+		return chengeHmFormatTime(startHm);
 	}
 
+
+	/**
+	 * startHm時間取得処理<br>
+	 * startHmの時間(hour)を取得する
+	 *
+	 * @param void
+	 * @return String startHmの時間(hour)
+	 */
+	public String getStartHmForHour() {
+		return startHm.substring(0, 2);
+	}
+
+
+	/**
+	 * startHm分取得処理<br>
+	 * startHmの分(minutes)を取得する
+	 *
+	 * @param void
+	 * @return String startHmの分(minutes)
+	 */
+	public String getStartHmForMinutes() {
+		return startHm.substring(2, 4);
+	}
+
+
+	/**
+	 * endHm時間フォーマット変換処理<br>
+	 * 時間フォーマット(HH:MM)に変換
+	 *
+	 * @param void
+	 * @return String 時間フォーマット(HH:MM)
+	 */
 	public String endHmsFormatTime() {
-		return hmFormatTime(endHm);
+		return chengeHmFormatTime(endHm);
 	}
 
+
+	/**
+	 * endHm時間取得処理<br>
+	 * endHmの時間(hour)を取得する
+	 *
+	 * @param void
+	 * @return String endHmの時間(hour)
+	 */
+	public String getEndHmForHour() {
+		return endHm.substring(0, 2);
+	}
+
+
+	/**
+	 * endHm分取得処理<br>
+	 * endHmの分(minutes)を取得する
+	 *
+	 * @param void
+	 * @return String endHmの分(minutes)
+	 */
+	public String getEndHmForMinutes() {
+		return endHm.substring(2, 4);
+	}
+
+
+	/**
+	 * restHm時間フォーマット変換処理<br>
+	 * 時間フォーマット(HH:MM)に変換
+	 *
+	 * @param void
+	 * @return String 時間フォーマット(HH:MM)
+	 */
 	public String restHmsFormatTime() {
-		return hmFormatTime(restHm);
+		return chengeHmFormatTime(restHm);
 	}
 
-	//private共通処理
-	private String hmFormatTime(String hm) {
 
-		//フォーマットをhh:mmに変換する
+	/**
+	 * restHm時間取得処理<br>
+	 * restHmの時間(hour)を取得する
+	 *
+	 * @param void
+	 * @return String restHmの時間(hour)
+	 */
+	public String getRestHmForHour() {
+		return restHm.substring(0, 2);
+	}
+
+
+	/**
+	 * restHm分取得処理<br>
+	 * restHmの分(minutes)を取得する
+	 *
+	 * @param void
+	 * @return String restHmの分(minutes)
+	 */
+	public String getRestHmForMinutes() {
+		return restHm.substring(2, 4);
+	}
+
+
+
+	/**
+	 * [private共通処理] 時間フォーマット変換処理<br>
+	 * 時間フォーマット(HH:MM)に変換
+	 *
+	 * @param hm 変換したい値
+	 * @return String 時間フォーマット(HH:MM)
+	 */
+	private String chengeHmFormatTime(String hm) {
+
+		//フォーマットをhh:mmに変換し、返す
 		return hm.substring(0, 2) + ":" + hm.substring(2, 4);
 	}
 }
