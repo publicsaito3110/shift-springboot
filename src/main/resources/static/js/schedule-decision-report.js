e02d59222939c2636c250773fbb026e80e616086
publicsaito3110 <public.business.saito3110@gmail.com>
2022-11-11 16:59:47 +0900

    fix: 勤務時間追加表示機能(非同期)の追加に伴う修正

--- src/main/resources/static/js/schedule-decision-report.js
+++ src/main/resources/static/js/schedule-decision-report.js
@@ -1,19 +1,36 @@
 $(function() {
 
 
-	//-------------------------------
-	//年月フォーム入力時の処理
-	//-------------------------------
-	$("#schedule-report-seach-month-input").change(function(){
+	//---------------------------------
+	//勤務日数年月フォーム入力時の処理
+	//---------------------------------
+	$("#schedule-report-seach-count-month-input").change(function(){
 
 		if ($(this).val().match(/^[0-9]{4}-(0[1-9]|1[0-2])$/)) {
 
 			//年月が指定されているとき検索ボタンを表示
-			$("#schedule-report-seach-btn").prop("disabled", false);
+			$("#schedule-report-seach-count-btn").prop("disabled", false);
 		} else {
 
 			//年月が指定されていないとき検索ボタンを非表示
-			$("#schedule-report-seach-btn").prop("disabled", true);
+			$("#schedule-report-seach-count-btn").prop("disabled", true);
+		}
+	});
+
+
+	//---------------------------------
+	//勤務時間年月フォーム入力時の処理
+	//---------------------------------
+	$("#schedule-report-seach-time-month-input").change(function(){
+
+		if ($(this).val().match(/^[0-9]{4}-(0[1-9]|1[0-2])$/)) {
+
+			//年月が指定されているとき検索ボタンを表示
+			$("#schedule-report-seach-time-btn").prop("disabled", false);
+		} else {
+
+			//年月が指定されていないとき検索ボタンを非表示
+			$("#schedule-report-seach-time-btn").prop("disabled", true);
 		}
 	});
 
@@ -23,16 +40,35 @@ $(function() {
 	//-------------------------------
 
 	//勤務日数追加ボタンを押下したとき、チェックボックス1をチェック
-	$("#schedule-report-seach-btn").on("click", function (){
+	$("#schedule-report-seach-count-btn").on("click", function (){
 
 		//<tr>タグを<tbody>に追加
-		$("#tbody-box").append("<tr></tr>");
+		$("#tbody-count").append("<tr></tr>");
 
 		//inputの値を取得し、ymフォーマットに変換
-		let ymDate = $("#schedule-report-seach-month-input").val();
+		let ymDate = $("#schedule-report-seach-count-month-input").val();
 		let ym = ymDate.replace("-", "");
 
 		//非同期処理後、テーブルに結果を追加
-		$("#tbody-box").children("tr:last").load("/schedule-decision/report/search?ym=" + ym + " .search-td1, .search-td2");
+		$("#tbody-count").children("tr:last").load("/schedule-decision/report/search-count?ym=" + ym + " .search-td1, .search-td2");
+	});
+
+
+	//-------------------------------
+	//勤務時間追加ボタン押下時の処理
+	//-------------------------------
+
+	//勤務日数追加ボタンを押下したとき、チェックボックス1をチェック
+	$("#schedule-report-seach-time-btn").on("click", function (){
+
+		//<tr>タグを<tbody>に追加
+		$("#tbody-time").append("<tr></tr>");
+
+		//inputの値を取得し、ymフォーマットに変換
+		let ymDate = $("#schedule-report-seach-time-month-input").val();
+		let ym = ymDate.replace("-", "");
+
+		//非同期処理後、テーブルに結果を追加
+		$("#tbody-time").children("tr:last").load("/schedule-decision/report/search-time?ym=" + ym + " .search-td1, .search-td2");
 	});
 });
