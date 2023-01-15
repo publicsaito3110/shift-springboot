cce89c2a01d64ed9e8c2d47c5824f02be0426da2
publicsaito3110 <public.business.saito3110@gmail.com>
2023-01-15 09:32:31 +0900

    fix: DM非同期処理の追加

--- src/main/resources/static/js/dm-talk.js
+++ src/main/resources/static/js/dm-talk.js
@@ -10,6 +10,40 @@ $(function(){
 		$("#chat-box").scrollTop(scrollHeight);
 	});
 
+
+	//-------------------------
+	//ページスクロール時の処理
+	//-------------------------
+	$('#chat-box').scroll(function() {
+
+		//現在のページの高さを取得
+		var nowScrollHeight = $(this).scrollTop();
+
+		//スクロールトップのとき
+		if(nowScrollHeight <= 0) {
+
+			//次のチャット件目がないとき
+			var nextLastOffset = $("#next-last-offset").val();
+			if (nextLastOffset <= 0) {
+				return;
+			}
+
+			//divタグを追加
+			$('#chat-box').prepend("<div></div>");
+			var receiveUser = $("#receiveUser").val();
+
+			//スクロールの高さを1pxに設定
+			$(this).scrollTop(1);
+
+			//非同期通信
+			$("#chat-box").children("div:first").load("/dm/talk/road?receiveUser=" + receiveUser + "&nextLastOffset=" + nextLastOffset + " .chat-list")
+
+			//最終チャット件目を更新
+			var chatLimit = $("#chat-limit").val();
+			$("#next-last-offset").val(nextLastOffset - chatLimit);
+		}
+	});
+
 	//-------------------------------
 	//メッセージテキスト入力時の処理
 	//-------------------------------
